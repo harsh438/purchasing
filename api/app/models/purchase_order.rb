209c1019710befe39a84513a21068709dbc1c0e4
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-10-28 14:04:10 +0000

    Add model aliases

--- api/app/models/purchase_order.rb
+++ api/app/models/purchase_order.rb
@@ -1,12 +1,28 @@
 class PurchaseOrder < ActiveRecord::Base
-  alias_attribute :product_id, :pID
-  alias_attribute :order_id, :oID
+  alias_attribute :product_id, :pID #foreign
+  alias_attribute :option_id, :oID #foreign
   alias_attribute :quantity, :qty
+  # qtyAdded
   alias_attribute :quantity_done, :qtyDone
+  # status
   alias_attribute :added_at, :added
+  # order_date
+  # drop_date
+  # arrived_date
   alias_attribute :invoice_payable_date, :inv_date
-  alias_attribute :summary_id, :po_number
+  alias_attribute :summary_id, :po_number #foreign
+  # operator
+  # comment
+  # cost
+  # report_status
+  # cancelled_date
+  # spare2
   alias_attribute :in_pvx, :inPVX
+  alias_attribute :season, :po_season
+  # reporting_pID
+  # original_pID
+  # original_oID
+  # orderToolLine
   alias_attribute :order_tool_rc, :orderTool_RC
   alias_attribute :order_tool_lg, :orderTool_LG
   alias_attribute :order_tool_ven_id, :orderTool_venID
@@ -16,6 +32,9 @@ class PurchaseOrder < ActiveRecord::Base
   alias_attribute :order_tool_sd_size, :orderTool_SDsize
   alias_attribute :order_tool_barcode, :orderTool_barcode
   alias_attribute :order_tool_sell_price, :orderTool_sellPrice
+  # orderTool_sizeSort
+  # orderTool_MultiLineID
+  # orderTool_SingleLineID
   alias_attribute :order_tool_brand_size, :orderTool_brandSize
   alias_attribute :order_tool_supplier_list_price, :orderTool_SupplierListPrice
   alias_attribute :order_tool_rrp, :orderTool_RRP
