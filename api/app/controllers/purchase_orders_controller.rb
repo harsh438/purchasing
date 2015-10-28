f409c8c604a5dd6f26b17de42ec7aa67f7637cbc
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-10-28 14:35:58 +0000

    Add endpoint

--- api/app/controllers/purchase_orders_controller.rb
+++ api/app/controllers/purchase_orders_controller.rb
@@ -1,2 +1,13 @@
 class PurchaseOrdersController < ApplicationController
+  before_action :load_purchase_orders
+
+  def index
+    render json: @purchase_orders
+  end
+
+  private
+
+  def load_purchase_orders
+    @purchase_orders = PurchaseOrder.first(10)
+  end
 end
