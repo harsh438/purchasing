664b88ec6a55a1d16221cf2a6337992f4d0159a1
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-10-28 11:16:31 +0000

    Fix tests

--- api/spec/server_spec.rb
+++ api/spec/server_spec.rb
@@ -2,7 +2,7 @@ describe 'Sinatra server' do
   context 'Visiting the homepage' do
     it 'should return a response' do
       visit '/api/purchase_orders.json'
-      expect(page.body).to include('Purchasing')
+      expect(page.body).to include(PurchaseOrder.first(5).to_json)
     end
   end
 end
