40f43be4a6eac5df6e86c52b56c93b47721b389c
Luke Morton <lukemorton.dev@gmail.com>
2015-10-28 10:57:17 +0000

    Serve index.html as root

--- api/app/server.rb
+++ api/app/server.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 require_relative 'database'
 require_relative 'models/purchase_order'
 
+set :root, File.dirname(__FILE__) + '/../'
+
 get '/' do
+  File.read(File.join('public', 'index.html'))
+end
+
+get '/api/purchase_orders.json' do
   content_type :json
   PurchaseOrder.first(5).to_json
 end
--- api/spec/server_spec.rb
+++ api/spec/server_spec.rb
@@ -3,7 +3,7 @@ require "#{File.dirname(__FILE__)}/helpers/spec_helper.rb"
 describe 'Sinatra server' do
   context 'Visiting the homepage' do
     it 'should return a response' do
-      visit '/'
+      visit '/api/purchase_orders.json'
       expect(page.body).to include('Purchasing')
     end
   end
