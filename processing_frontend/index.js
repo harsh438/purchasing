977d61582a27ea4a69ab4f6bcc429c8e1b0bbaff
Luke Morton <lukemorton.dev@gmail.com>
2015-10-29 10:46:53 +0000

    Move application code into app/

--- processing_frontend/index.js
+++ processing_frontend/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
 import createHistory from 'history/lib/createBrowserHistory';
-import routes from './routes';
+import routes from './app/routes';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './app/store';
 
 ReactDOM.render((
   <Provider store={store}>
