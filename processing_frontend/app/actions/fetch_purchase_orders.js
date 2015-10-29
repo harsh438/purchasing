5522aa820ce29c292d6709ff444a5ea7dcf883bc
Luke Morton <lukemorton.dev@gmail.com>
2015-10-29 14:47:57 +0000

    Sort by ID desc for now

--- processing_frontend/app/actions/fetch_purchase_orders.js
+++ processing_frontend/app/actions/fetch_purchase_orders.js
@@ -8,7 +8,7 @@ function appendPurchaseOrders({ page, purchaseOrders }) {
 
 export default function fetchPurchaseOrders(page = 1) {
   return dispatch => {
-    fetch(`/api/purchase_orders.json?page=${page}`)
+    fetch(`/api/purchase_orders.json?sort_field=id&sort_dir=desc&page=${page}`)
       .then(response => response.json())
       .then(purchaseOrders => dispatch(appendPurchaseOrders({ page, purchaseOrders })));
   }
