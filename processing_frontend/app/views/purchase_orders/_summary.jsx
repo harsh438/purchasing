a55af621b5763ee15f37968d66d348ef93ce5e5a
Luke Morton <lukemorton.dev@gmail.com>
2015-10-29 22:48:57 +0000

    Style up mock summary

--- processing_frontend/app/views/purchase_orders/_summary.jsx
+++ processing_frontend/app/views/purchase_orders/_summary.jsx
@@ -11,16 +11,39 @@ export default class PurchaseOrdersSummary extends React.Component {
           </div>
 
           <div className="panel-body">
-            <div className="purchase_orders_stats">
+            <div className="purchase_orders_summary">
               <table className="table table-striped">
                 <thead>
                   <tr>
-                    <th>Total</th>
+                    <th>&nbsp;</th>
+                    <th colSpan="2" style={{ borderLeft: '2px solid #ddd' }}>Order</th>
+                    <th colSpan="2" style={{ borderLeft: '2px solid #ddd' }}>Delivery</th>
+                    <th colSpan="2" style={{ borderLeft: '2px solid #ddd' }}>Cancel</th>
+                    <th colSpan="2" style={{ borderLeft: '2px solid #ddd' }}>Balance</th>
+                  </tr>
+                  <tr>
+                    <th>&nbsp;</th>
+                    <th style={{ borderLeft: '2px solid #ddd' }}>Cost</th>
+                    <th>Value</th>
+                    <th style={{ borderLeft: '2px solid #ddd' }}>Cost</th>
+                    <th>Value</th>
+                    <th style={{ borderLeft: '2px solid #ddd' }}>Cost</th>
+                    <th>Value</th>
+                    <th style={{ borderLeft: '2px solid #ddd' }}>Cost</th>
+                    <th>Value</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <th>100</th>
+                    <th>Total</th>
+                    <td style={{ borderLeft: '2px solid #ddd' }}>£100</td>
+                    <td>£1,000</td>
+                    <td style={{ borderLeft: '2px solid #ddd' }}>£100</td>
+                    <td>£10,000</td>
+                    <td style={{ borderLeft: '2px solid #ddd' }}>£100</td>
+                    <td>£1,000</td>
+                    <td style={{ borderLeft: '2px solid #ddd' }}>£100</td>
+                    <td>£10,000</td>
                   </tr>
                 </tbody>
               </table>
