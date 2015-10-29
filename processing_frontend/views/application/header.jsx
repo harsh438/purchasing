ba2107516cd1e2e78737a38cf9a5f80d2e0081f2
Luke Morton <lukemorton.dev@gmail.com>
2015-10-29 10:00:39 +0000

    Implement header which also required setting up images

---
+++ processing_frontend/views/application/header.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class HeaderLink extends React.Component {
+  render () {
+    return (
+      <li className={this.className()}>
+        <Link to={this.props.to}>{this.props.text}</Link>
+      </li>
+    );
+  }
+
+  className() {
+    return '';
+  }
+}
+
+export default class Header extends React.Component {
+  render () {
+    return (
+      <div className="container-fluid">
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <Link to="/"
+                    className="navbar-brand"
+                    style={{ paddingTop: '.6em' }}>
+                <img src="/assets/images/surfdome-logo-white-500px.png"
+                     alt="Surfdome"
+                     style={{ height: '30px'}} />
+              </Link>
+            </div>
+
+            <ul className="nav navbar-nav">
+              <HeaderLink to="/" text="Purchase Orders" />
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
--- processing_frontend/views/layouts/application.jsx
+++ processing_frontend/views/layouts/application.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
+import Header from '../application/header';
 
 export default class Application extends React.Component {
   render () {
     return (
       <div className="app">
+        <Header />
         {this.props.children}
       </div>
     );
