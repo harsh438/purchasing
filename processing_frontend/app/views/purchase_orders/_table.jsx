deaf3c207ed8052e3111fda928ef6f8900e74de4
Luke Morton <lukemorton.dev@gmail.com>
2015-10-29 12:48:20 +0000

    Add more columns to purchase order table

--- processing_frontend/app/views/purchase_orders/_table.jsx
+++ processing_frontend/app/views/purchase_orders/_table.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 
 class PurchaseOrderRow extends React.Component {
   render () {
+    const orderId = this.props.purchaseOrder.id;
+    const poNumber = this.props.purchaseOrder.summary_id;
     const productId = this.props.purchaseOrder.product_id;
     const productName = this.props.purchaseOrder.product_name;
+    const productSKU = this.props.purchaseOrder.product_sku;
+    const productCost = this.props.purchaseOrder.cost;
+    const productSize = this.props.purchaseOrder.product_size;
+    const operator = this.props.purchaseOrder.operator;
 
     return (
       <tr>
+        <td>{orderId}</td>
+        <td>{poNumber}</td>
         <td>{productId}</td>
         <td>{productName}</td>
+        <td>{productSKU}</td>
+        <td>{productCost}</td>
+        <td>{productSize}</td>
+        <td>{operator}</td>
       </tr>
     );
   }
@@ -17,22 +29,31 @@ class PurchaseOrderRow extends React.Component {
 export default class PurchaseOrdersTable extends React.Component {
   render () {
     return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th style={{ width: '10%' }}>Product ID</th>
-            <th>Product Name</th>
-          </tr>
-        </thead>
-        <tbody>{this.rows()}</tbody>
-      </table>
+      <div className="purchase_orders_table">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Order #</th>
+              <th>PO #</th>
+              <th>PID</th>
+              <th>Product</th>
+              <th>SKU</th>
+              <th>Unit Price</th>
+              <th>Size</th>
+              <th>Operator</th>
+            </tr>
+          </thead>
+          <tbody>{this.rows()}</tbody>
+        </table>
+      </div>
     );
   }
 
   rows () {
     return this.props.purchaseOrders.map(function (purchaseOrder) {
       return (
-        <PurchaseOrderRow key={purchaseOrder.id} purchaseOrder={purchaseOrder} />
+        <PurchaseOrderRow key={purchaseOrder.id}
+                          purchaseOrder={purchaseOrder} />
       );
     });
   }
