b33bd8925b5a0f3d29be7dffe23caf3c60c9fde8
Luke Morton <lukemorton.dev@gmail.com>
2015-10-30 15:08:39 +0000

    Reenable auth

--- api/config/application.rb
+++ api/config/application.rb
@@ -11,7 +11,7 @@ module Purchasing
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('lib')
 
-    config.http_auth = { enabled: false,
+    config.http_auth = { enabled: true,
                          username: 'purchasing',
                          password: 'lastordersplease' }
   end
