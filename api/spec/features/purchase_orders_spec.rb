ab58f0671250956232b62cbdf69b9c721f49f095
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-10-30 09:48:49 +0000

    Add specs

--- api/spec/features/purchase_orders_spec.rb
+++ api/spec/features/purchase_orders_spec.rb
@@ -3,9 +3,9 @@ feature 'Listing purchase orders' do
   let (:vendor) { create(:vendor) }
 
   before(:each) do
-    create_list(:purchase_order, 20)
+    create_list(:purchase_order, 20, status: 3)
     create_list(:purchase_order, 16, :arrived)
-    create_list(:purchase_order, 15, vendor: vendor)
+    create_list(:purchase_order, 15, vendor: vendor, status: -1)
   end
 
   scenario 'Default purchase order list' do
@@ -21,6 +21,16 @@ feature 'Listing purchase orders' do
   scenario 'Filtering by vendor' do
     when_i_filter_by_vendor
     then_i_should_see_the_first_page_of_orders_for_that_vendor
+  end
+
+  scenario 'Filtering by status' do
+    when_i_filter_by_status
+    then_i_should_see_the_first_page_of_orders_with_that_status
+  end
+
+  scenario 'Filtering by multiple statuses' do
+    when_i_filter_by_multiple_statuses
+    then_i_should_see_the_first_page_of_orders_with_those_statuses
   end
 end
 
@@ -46,4 +56,20 @@ end
 
 def then_i_should_see_one_result
   expect(subject.count).to eq(1)
+end
+
+def when_i_filter_by_status
+  visit '/api/purchase_orders.json?status=3'
+end
+
+def then_i_should_see_the_first_page_of_orders_with_that_status
+  expect(subject.count).to eq(20)
+end
+
+def when_i_filter_by_multiple_statuses
+  visit '/api/purchase_orders.json?status[]=3&status[]=-1'
+end
+
+def then_i_should_see_the_first_page_of_orders_with_those_statuses
+  expect(subject.count).to eq(35)
 end
