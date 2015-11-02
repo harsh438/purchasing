02f18615e4cb8ba0a90f05a97b93b5f923361813
Luke Morton <lukemorton.dev@gmail.com>
2015-11-02 22:09:17 +0000

    Ensure protocol is https for production and staging

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   include HttpAuth
   protect_from_forgery with: :exception
   layout false
+
+  def default_url_options
+    if Rails.env.staging? or Rails.env.production?
+      { protocol: 'https' }
+    else
+      {}
+    end
+  end
 end
