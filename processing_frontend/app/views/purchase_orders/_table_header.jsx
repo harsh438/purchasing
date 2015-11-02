dbcbbbab8c31ed8b92ab1b8f0fd9ab54dcb49be7
Luke Morton <lukemorton.dev@gmail.com>
2015-11-02 21:30:31 +0000

    Only show totals in headers if values present

--- processing_frontend/app/views/purchase_orders/_table_header.jsx
+++ processing_frontend/app/views/purchase_orders/_table_header.jsx
@@ -20,29 +20,29 @@ export default class PurchaseOrderTableHeader extends React.Component {
           <th colSpan="6" style={{ borderLeft: '2px solid #ddd' }}>
             Ordered
 
-            <div style={{ fontWeight: 'normal' }}>Total cost: £{this.props.summary.orderedCost}</div>
-            <div style={{ fontWeight: 'normal' }}>Total value: £{this.props.summary.orderedValue}</div>
+            {this.renderTotalCost(this.props.summary.orderedCost)}
+            {this.renderTotalValue(this.props.summary.orderedValue)}
           </th>
 
           <th colSpan="4" style={{ borderLeft: '2px solid #ddd' }}>
             Delivered
 
-            <div style={{ fontWeight: 'normal' }}>Total cost: £{this.props.summary.deliveredCost}</div>
-            <div style={{ fontWeight: 'normal' }}>Total value: £{this.props.summary.deliveredValue}</div>
+            {this.renderTotalCost(this.props.summary.deliveredCost)}
+            {this.renderTotalValue(this.props.summary.deliveredValue)}
           </th>
 
           <th colSpan="3" style={{ borderLeft: '2px solid #ddd' }}>
             Cancelled
 
-            <div style={{ fontWeight: 'normal' }}>Total cost: £{this.props.summary.cancelledCost}</div>
-            <div style={{ fontWeight: 'normal' }}>Total value: £{this.props.summary.cancelledValue}</div>
+            {this.renderTotalCost(this.props.summary.cancelledCost)}
+            {this.renderTotalValue(this.props.summary.cancelledValue)}
           </th>
 
           <th colSpan="3" style={{ borderLeft: '2px solid #ddd' }}>
             Balance
 
-            <div style={{ fontWeight: 'normal' }}>Total cost: £{this.props.summary.balanceCost}</div>
-            <div style={{ fontWeight: 'normal' }}>Total value: £{this.props.summary.balanceValue}</div>
+            {this.renderTotalCost(this.props.summary.balanceCost)}
+            {this.renderTotalValue(this.props.summary.balanceValue)}
           </th>
 
           <th colSpan="6" style={{ borderLeft: '2px solid #ddd' }}>
@@ -115,6 +115,22 @@ export default class PurchaseOrderTableHeader extends React.Component {
         <strong>Result set too big to export :(</strong>
       );
     }
+  }
+
+  renderTotalCost (totalCost) {
+    if (!totalCost) return;
+
+    return (
+      <div style={{ fontWeight: 'normal' }}>Total cost: £{totalCost}</div>
+    );
+  }
+
+  renderTotalValue (totalValue) {
+    if (!totalValue) return;
+
+    return (
+      <div style={{ fontWeight: 'normal' }}>Total cost: £{totalValue}</div>
+    );
   }
 
   fixCellWidths () {
