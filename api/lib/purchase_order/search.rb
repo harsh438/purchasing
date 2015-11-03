56d521f7977074394c57ac3039c3cad304c730a3
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-03 15:28:39 +0000

    Speed up summaries

--- api/lib/purchase_order/search.rb
+++ api/lib/purchase_order/search.rb
@@ -5,7 +5,6 @@ class PurchaseOrder::Search
     filters = Filters.new(PurchaseOrder, attrs)
     unpaged_results = filters.filter(PurchaseOrder.mapped.with_valid_status.with_summary)
     results = unpaged_results.page(attrs[:page])
-
     { summary: {},
       results: results,
       more_results_available: !results.last_page?,
@@ -29,18 +28,43 @@ class PurchaseOrder::Search
     number_to_currency(figure, unit: '£')
   end
 
-  def summarize(results)
-    { ordered_quantity: results.map { |r| r.quantity }.compact.sum.to_s,
-      ordered_cost: monetize(results.map { |r| r.ordered_cost }.compact.sum),
-      ordered_value: monetize(results.map { |r| r.ordered_value }.compact.sum),
-      delivered_quantity: results.map { |r| r.delivered_quantity }.compact.sum.to_s,
-      delivered_cost: monetize(results.map { |r| r.delivered_cost }.compact.sum),
-      delivered_value: monetize(results.map { |r| r.delivered_value }.compact.sum),
-      cancelled_quantity: results.map { |r| r.cancelled_quantity }.compact.sum.to_s,
-      cancelled_cost: monetize(results.map { |r| r.cancelled_cost }.compact.sum),
-      cancelled_value: monetize(results.map { |r| r.cancelled_value }.compact.sum),
-      balance_quantity: results.map { |r| r.balance_quantity }.compact.sum.to_s,
-      balance_cost: monetize(results.map { |r| r.balance_cost }.compact.sum),
-      balance_value: monetize(results.map { |r| r.balance_value }.compact.sum) }
+  def summarize(unpaged_results)
+    r = unpaged_results
+          .joins('inner join ds_products p on purchase_orders.pID = p.pID')
+
+    ordered_quantity, ordered_cost, ordered_value, delivered_quantity,
+    delivered_cost, delivered_value, balance_quantity, balance_cost, balance_value =
+      r.pluck('sum(qty) as ordered_quantity,
+                 sum(qty * cost) as ordered_cost,
+                 sum(qty * p.pPrice) as ordered_value,
+
+                 sum((qtyDone + qtyAdded)) as delivered_quantity,
+                 sum((qtyDone + qtyAdded) * cost) as delivered_cost,
+                 sum((qtyDone + qtyAdded) * p.pPrice) as delivered_value,
+
+                 sum(qty - (qtyDone + qtyAdded)) as balance_quantity,
+                 (sum(qty * cost) - sum((qtyDone + qtyAdded) * cost)) as balance_cost,
+                 (sum(qty * p.pPrice) - sum((qtyDone + qtyAdded) * p.pPrice)) as balance_value')
+       .flatten
+
+    cancelled_quantity, cancelled_cost, cancelled_value =
+      r.where('status = -1')
+       .pluck('sum((qty - (qtyDone + qtyAdded))) as cancelled_quantity,
+               sum((qty - (qtyDone + qtyAdded)) * cost),
+               sum((qty - (qtyDone + qtyAdded)) * p.pPrice)')
+       .flatten
+
+    { ordered_quantity: ordered_quantity,
+      ordered_cost: monetize(ordered_cost),
+      ordered_value: monetize(ordered_value),
+      delivered_quantity: delivered_quantity,
+      delivered_cost: monetize(delivered_cost),
+      delivered_value: monetize(delivered_value),
+      balance_quantity: balance_quantity,
+      balance_cost: monetize(balance_cost),
+      balance_value: monetize(balance_value),
+      cancelled_quantity: cancelled_quantity,
+      cancelled_cost: monetize(cancelled_cost),
+      cancelled_value: monetize(cancelled_value) }
   end
 end
