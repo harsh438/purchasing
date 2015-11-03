22d2b4f0f23335c435e133d2b206c00c635963ae
Luke Morton <lukemorton.dev@gmail.com>
2015-11-03 12:33:22 +0000

    Tidy up

--- processing_frontend/app/actions/filters.js
+++ processing_frontend/app/actions/filters.js
@@ -1,77 +1,31 @@
-function setBrands(brands) {
-  return { type: 'SET_BRANDS',
-           brands: brands };
-}
-
-function setCategories(categories) {
-  return { type: 'SET_CATEGORIES',
-           categories: categories };
-}
-
-function setGenders(genders) {
-  return { type: 'SET_GENDERS',
-           genders: genders };
-}
-
-function setOrderTypes(orderTypes) {
-  return { type: 'SET_ORDER_TYPES',
-           orderTypes: orderTypes };
-}
-
-function setSeasons(seasons) {
-  return { type: 'SET_SEASONS',
-           seasons: seasons };
-}
-
-function setSuppliers(suppliers) {
-  return { type: 'SET_SUPPLIERS',
-           suppliers: suppliers };
+function loadFilters(type, action) {
+  return dispatch => {
+    fetch(`/api/${type}.json`, { credentials: 'same-origin' })
+      .then(response => response.json())
+      .then(filters => dispatch(action(filters)));
+  }
 }
 
 export function loadBrands() {
-  return dispatch => {
-    fetch('/api/vendors.json', { credentials: 'same-origin' })
-      .then(response => response.json())
-      .then(brands => dispatch(setBrands(brands)));
-  }
+  return loadFilters('vendors', brands => ({ brands, type: 'SET_BRANDS' }));
 }
 
 export function loadCategories() {
-  return dispatch => {
-    fetch('/api/categories.json', { credentials: 'same-origin' })
-      .then(response => response.json())
-      .then(categories => dispatch(setCategories(categories)));
-  }
+  return loadFilters('categories', categories => ({ categories, type: 'SET_CATEGORIES' }));
 }
 
 export function loadGenders() {
-  return dispatch => {
-    fetch('/api/genders.json', { credentials: 'same-origin' })
-      .then(response => response.json())
-      .then(genders => dispatch(setGenders(genders)));
-  }
+  return loadFilters('genders', genders => ({ genders, type: 'SET_GENDERS' }));
 }
 
 export function loadOrderTypes() {
-  return dispatch => {
-    fetch('/api/order_types.json', { credentials: 'same-origin' })
-      .then(response => response.json())
-      .then(orderTypes => dispatch(setOrderTypes(orderTypes)));
-  }
+  return loadFilters('order_types', orderTypes => ({ orderTypes, type: 'SET_ORDER_TYPES' }));
 }
 
 export function loadSeasons() {
-  return dispatch => {
-    fetch('/api/seasons.json', { credentials: 'same-origin' })
-      .then(response => response.json())
-      .then(seasons => dispatch(setSeasons(seasons)));
-  }
+  return loadFilters('seasons', seasons => ({ seasons, type: 'SET_SEASONS' }));
 }
 
 export function loadSuppliers() {
-  return dispatch => {
-    fetch('/api/suppliers.json', { credentials: 'same-origin' })
-      .then(response => response.json())
-      .then(suppliers => dispatch(setSuppliers(suppliers)));
-  }
+  return loadFilters('suppliers', suppliers => ({ suppliers, type: 'SET_SUPPLIERS' }));
 }
