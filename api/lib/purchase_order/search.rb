db9a2fc514200556b4dc58922631a3dc7210afcd
Luke Morton <lukemorton.dev@gmail.com>
2015-11-03 10:23:36 +0000

    Add quantity to summary info

--- api/lib/purchase_order/search.rb
+++ api/lib/purchase_order/search.rb
@@ -24,12 +24,16 @@ class PurchaseOrder::Search
   end
 
   def summarize(results)
-    { ordered_cost: results.map { |r| r.ordered_cost }.compact.sum,
+    { ordered_quantity: results.map { |r| r.ordered_quantity }.compact.sum,
+      ordered_cost: results.map { |r| r.ordered_cost }.compact.sum,
       ordered_value: results.map { |r| r.ordered_value }.compact.sum,
+      delivered_quantity: results.map { |r| r.delivered_quantity }.compact.sum,
       delivered_cost: results.map { |r| r.delivered_cost }.compact.sum,
       delivered_value: results.map { |r| r.delivered_value }.compact.sum,
+      cancelled_quantity: results.map { |r| r.cancelled_quantity }.compact.sum,
       cancelled_cost: results.map { |r| r.cancelled_cost }.compact.sum,
       cancelled_value: results.map { |r| r.cancelled_value }.compact.sum,
+      balance_quantity: results.map { |r| r.balance_quantity }.compact.sum,
       balance_cost: results.map { |r| r.balance_cost }.compact.sum,
       balance_value: results.map { |r| r.balance_value }.compact.sum }
   end
