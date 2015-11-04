726c16545fab3115fc8d34eabbeda01f6220335e
Luke Morton <lukemorton.dev@gmail.com>
2015-11-04 09:38:05 +0000

    Move drop number logic into PurchaseOrder::DropNumbers

---
+++ api/lib/purchase_order/drop_numbers.rb
@@ -0,0 +1,57 @@
+class PurchaseOrder::DropNumbers
+  def calculate(results)
+    criteria = results.reduce([]) do |criteria, purchase_order|
+      if purchase_order.product_id > 0 and purchase_order.option_id > 0
+        criteria << purchase_order.product_id
+        criteria << purchase_order.option_id
+      else
+        criteria
+      end
+    end
+
+    return [] unless criteria.count > 0
+
+    query = (criteria.count / 2).times.map { '(pID = ? AND oID = ?)' }.join(' OR ')
+
+    total_drops = PurchaseOrder.with_summary
+                               .with_valid_status
+                               .where(query, *criteria)
+                               .group(:pID, :oID)
+                               .count
+
+    criteria = results.reduce([]) do |criteria, purchase_order|
+      if purchase_order.product_id > 0 and purchase_order.option_id > 0
+        criteria << purchase_order.product_id
+        criteria << purchase_order.option_id
+        criteria << purchase_order.drop_date
+      else
+        criteria
+      end
+    end
+
+    query = (criteria.count / 3).times.map { '(pID = ? AND oID = ? AND drop_date < ?)' }.join(' OR ')
+
+    drops_before = PurchaseOrder.with_summary
+                                .with_valid_status
+                                .where(query, *criteria)
+                                .group(:drop_date, :pID, :oID)
+                                .count
+
+    drops = {}
+
+    results.each do |purchase_order|
+      drops_before_key = [purchase_order.delivery_date,
+                          purchase_order.product_id,
+                          purchase_order.option_id]
+
+      total_drops_key = [purchase_order.product_id,
+                         purchase_order.option_id]
+
+      drops_before_value = (drops_before[drops_before_key] || 0) + 1
+
+      drops[purchase_order.id] = "#{drops_before_value}/#{total_drops[total_drops_key]}"
+    end
+
+    drops
+  end
+end
--- api/lib/purchase_order/search.rb
+++ api/lib/purchase_order/search.rb
@@ -6,11 +6,9 @@ class PurchaseOrder::Search
     unpaged_results = filters.filter(PurchaseOrder.mapped.with_valid_status.with_summary)
     results = unpaged_results.page(attrs[:page])
 
-
-
     { summary: {},
       results: results,
-      drop_dates: drop_dates(results),
+      drop_numbers: PurchaseOrder::DropNumbers.new.calculate(results),
       more_results_available: !results.last_page?,
       total_count: results.total_count,
       total_pages: results.total_pages,
@@ -70,61 +68,5 @@ class PurchaseOrder::Search
       cancelled_quantity: cancelled_quantity,
       cancelled_cost: monetize(cancelled_cost),
       cancelled_value: monetize(cancelled_value) }
-  end
-
-  def drop_dates(results)
-    criteria = results.reduce([]) do |criteria, purchase_order|
-      if purchase_order.product_id > 0 and purchase_order.option_id > 0
-        criteria << purchase_order.product_id
-        criteria << purchase_order.option_id
-      else
-        criteria
-      end
-    end
-
-    return [] unless criteria.count > 0
-
-    query = (criteria.count / 2).times.map { '(pID = ? AND oID = ?)' }.join(' OR ')
-
-    total_drops = PurchaseOrder.with_summary
-                               .with_valid_status
-                               .where(query, *criteria)
-                               .group(:pID, :oID)
-                               .count
-
-    criteria = results.reduce([]) do |criteria, purchase_order|
-      if purchase_order.product_id > 0 and purchase_order.option_id > 0
-        criteria << purchase_order.product_id
-        criteria << purchase_order.option_id
-        criteria << purchase_order.drop_date
-      else
-        criteria
-      end
-    end
-
-    query = (criteria.count / 3).times.map { '(pID = ? AND oID = ? AND drop_date < ?)' }.join(' OR ')
-
-    drops_before = PurchaseOrder.with_summary
-                                .with_valid_status
-                                .where(query, *criteria)
-                                .group(:drop_date, :pID, :oID)
-                                .count
-
-    drops = {}
-
-    results.each do |purchase_order|
-      drops_before_key = [purchase_order.delivery_date,
-                          purchase_order.product_id,
-                          purchase_order.option_id]
-
-      total_drops_key = [purchase_order.product_id,
-                         purchase_order.option_id]
-
-      drops_before_value = (drops_before[drops_before_key] || 0) + 1
-
-      drops[purchase_order.id] = "#{drops_before_value}/#{total_drops[total_drops_key]}"
-    end
-
-    drops
   end
 end
