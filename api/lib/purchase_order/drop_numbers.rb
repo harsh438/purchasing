fc83bf6709e10404aafd8147842c80436699d689
Luke Morton <lukemorton.dev@gmail.com>
2015-11-04 09:45:00 +0000

    Move finding of previous drops to helper method

--- api/lib/purchase_order/drop_numbers.rb
+++ api/lib/purchase_order/drop_numbers.rb
@@ -2,38 +2,22 @@ class PurchaseOrder::DropNumbers
   def calculate(results)
     total_drops = find_total_drops(results)
     return {} unless total_drops.any?
-
-    criteria = results.reduce([]) do |criteria, purchase_order|
-      if purchase_order.product_id > 0 and purchase_order.option_id > 0
-        criteria << purchase_order.product_id
-        criteria << purchase_order.option_id
-        criteria << purchase_order.drop_date
-      else
-        criteria
-      end
-    end
-
-    query = (criteria.count / 3).times.map { '(pID = ? AND oID = ? AND drop_date < ?)' }.join(' OR ')
-
-    drops_before = PurchaseOrder.with_summary
-                                .with_valid_status
-                                .where(query, *criteria)
-                                .group(:drop_date, :pID, :oID)
-                                .count
+    previous_drops = find_previous_drops(results)
+    return {} unless previous_drops.any?
 
     drops = {}
 
     results.each do |purchase_order|
-      drops_before_key = [purchase_order.delivery_date,
-                          purchase_order.product_id,
-                          purchase_order.option_id]
+      previous_drops_key = [purchase_order.delivery_date,
+                            purchase_order.product_id,
+                            purchase_order.option_id]
 
       total_drops_key = [purchase_order.product_id,
                          purchase_order.option_id]
 
-      drops_before_value = (drops_before[drops_before_key] || 0) + 1
+      previous_drops_value = (previous_drops[previous_drops_key] || 0) + 1
 
-      drops[purchase_order.id] = "#{drops_before_value}/#{total_drops[total_drops_key]}"
+      drops[purchase_order.id] = "#{previous_drops_value}/#{total_drops[total_drops_key]}"
     end
 
     drops
@@ -60,5 +44,27 @@ class PurchaseOrder::DropNumbers
                  .where(query, *criteria)
                  .group(:pID, :oID)
                  .count
+  end
+
+  def find_previous_drops(results)
+    criteria = results.reduce([]) do |criteria, purchase_order|
+      if purchase_order.product_id > 0 and purchase_order.option_id > 0
+        criteria << purchase_order.product_id
+        criteria << purchase_order.option_id
+        criteria << purchase_order.drop_date
+      else
+        criteria
+      end
+    end
+
+    return PurchaseOrder.none unless criteria.count > 0
+
+    query = (criteria.count / 3).times.map { '(pID = ? AND oID = ? AND drop_date < ?)' }.join(' OR ')
+
+    PurchaseOrder.with_summary
+                  .with_valid_status
+                  .where(query, *criteria)
+                  .group(:drop_date, :pID, :oID)
+                  .count
   end
 end
