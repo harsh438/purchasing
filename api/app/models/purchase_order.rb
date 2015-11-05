1c9416c12d485bd8e40852c340205463274bae92
Luke Morton <lukemorton.dev@gmail.com>
2015-11-05 16:55:11 +0000

    Move CSV column order and to_csv logic into PurchaseOrder::CsvExporter

--- api/app/models/purchase_order.rb
+++ api/app/models/purchase_order.rb
@@ -3,52 +3,6 @@ class PurchaseOrder < ActiveRecord::Base
   include LegacyMappings
   include Searchable
 
-  CSV_COLUMN_ORDER = %w(product_barcode
-                        po_number
-                        order_type
-                        product_id
-                        product_sku
-                        season
-                        brand
-                        category
-                        gender
-                        product_name
-                        product_size
-                        supplier_style_code
-                        supplier_color_code
-                        supplier_product_name
-                        supplier_color_name
-                        brand_size
-                        product_cost
-                        status
-                        order_id
-                        order_date
-                        ordered_quantity
-                        ordered_cost
-                        ordered_value
-                        delivery_date
-                        order_first_received
-                        delivered_quantity
-                        delivered_cost
-                        delivered_value
-                        cancelled_quantity
-                        cancelled_cost
-                        cancelled_value
-                        operator
-                        weeks_on_sale
-                        closing_date
-                        comment)
-
-  def self.to_csv
-    CSV.generate do |csv|
-      csv << CSV_COLUMN_ORDER.map(&:humanize)
-
-      where(nil).each do |purchase_order|
-        csv << purchase_order.as_json({}).values_at(*CSV_COLUMN_ORDER.map(&:to_sym))
-      end
-    end
-  end
-
   def self.filter_supplier(context)
     joins(vendor: :supplier_vendors)
       .where(suppliers_to_brands: { SupplierID: context[:supplier] })
--- api/lib/purchase_order/csv_exporter.rb
+++ api/lib/purchase_order/csv_exporter.rb
@@ -1,6 +1,55 @@
 class PurchaseOrder::CsvExporter
+  CSV_COLUMN_ORDER = %w(product_barcode
+                        po_number
+                        order_type
+                        product_id
+                        product_sku
+                        season
+                        brand
+                        category
+                        gender
+                        product_name
+                        product_size
+                        supplier_style_code
+                        supplier_color_code
+                        supplier_product_name
+                        supplier_color_name
+                        brand_size
+                        product_cost
+                        status
+                        order_id
+                        order_date
+                        ordered_quantity
+                        ordered_cost
+                        ordered_value
+                        delivery_date
+                        order_first_received
+                        delivered_quantity
+                        delivered_cost
+                        delivered_value
+                        cancelled_quantity
+                        cancelled_cost
+                        cancelled_value
+                        operator
+                        weeks_on_sale
+                        closing_date
+                        comment)
+
   def export(attrs)
     query = PurchaseOrder.mapped.with_valid_status.with_summary
-    PurchaseOrder::Filter.new.filter(query, attrs)
+    query = PurchaseOrder::Filter.new.filter(query, attrs)
+    to_csv(query)
+  end
+
+  private
+
+  def to_csv(query)
+    CSV.generate do |csv|
+      csv << CSV_COLUMN_ORDER.map(&:humanize)
+
+      query.each do |purchase_order|
+        csv << purchase_order.as_json({}).values_at(*CSV_COLUMN_ORDER.map(&:to_sym))
+      end
+    end
   end
 end
