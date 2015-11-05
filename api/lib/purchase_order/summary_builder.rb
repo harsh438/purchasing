c0a145d5254ee4802d73cc68cd967dac457d688d
Luke Morton <lukemorton.dev@gmail.com>
2015-11-05 15:04:03 +0000

    Separate ordered summary from delivered/balanced quantity

--- api/lib/purchase_order/summary_builder.rb
+++ api/lib/purchase_order/summary_builder.rb
@@ -5,23 +5,27 @@ class PurchaseOrder::SummaryBuilder
     r = unpaged_results
           .joins('inner join ds_products p on purchase_orders.pID = p.pID')
 
-    ordered_quantity, ordered_cost, ordered_value, delivered_quantity,
-    delivered_cost, delivered_value, balance_quantity, balance_cost, balance_value =
-      r.pluck('sum(qty) as ordered_quantity,
-                 sum(qty * cost) as ordered_cost,
-                 sum(qty * p.pPrice) as ordered_value,
+    ordered_quantity, ordered_cost, ordered_value =
+      r.where(status: [-1, 2, 3, 4, 5])
+       .pluck('sum(qty) as ordered_quantity,
+               sum(qty * cost) as ordered_cost,
+               sum(qty * p.pPrice) as ordered_value')
+       .flatten
 
-                 sum((qtyDone + qtyAdded)) as delivered_quantity,
-                 sum((qtyDone + qtyAdded) * cost) as delivered_cost,
-                 sum((qtyDone + qtyAdded) * p.pPrice) as delivered_value,
+    delivered_quantity, delivered_cost, delivered_value,
+    balance_quantity, balance_cost, balance_value =
+      r.where.not(status: -1)
+       .pluck('sum((qtyDone + qtyAdded)) as delivered_quantity,
+               sum((qtyDone + qtyAdded) * cost) as delivered_cost,
+               sum((qtyDone + qtyAdded) * p.pPrice) as delivered_value,
 
-                 sum(qty - (qtyDone + qtyAdded)) as balance_quantity,
-                 (sum(qty * cost) - sum((qtyDone + qtyAdded) * cost)) as balance_cost,
-                 (sum(qty * p.pPrice) - sum((qtyDone + qtyAdded) * p.pPrice)) as balance_value')
+               sum(qty - (qtyDone + qtyAdded)) as balance_quantity,
+               (sum(qty * cost) - sum((qtyDone + qtyAdded) * cost)) as balance_cost,
+               (sum(qty * p.pPrice) - sum((qtyDone + qtyAdded) * p.pPrice)) as balance_value')
        .flatten
 
     cancelled_quantity, cancelled_cost, cancelled_value =
-      r.where('status = -1')
+      r.where(status: -1)
        .pluck('sum((qty - (qtyDone + qtyAdded))) as cancelled_quantity,
                sum((qty - (qtyDone + qtyAdded)) * cost),
                sum((qty - (qtyDone + qtyAdded)) * p.pPrice)')
