d3bc43aeb17daf63f55d484e7fa61a441bef75ad
Luke Morton <lukemorton.dev@gmail.com>
2015-11-09 11:10:01 +0000

    Tidy up cancel purchase order spec

--- api/spec/features/cancel_purchase_orders_spec.rb
+++ api/spec/features/cancel_purchase_orders_spec.rb
@@ -3,24 +3,7 @@ feature 'Cancelling purchase orders' do
   let (:vendor) { create(:vendor) }
 
   before(:each) do
-    create_list(:purchase_order, 20,
-                status: 4,
-                season: 'AW15',
-                po_number: 123,
-                delivery_date: Time.new(2013, 1, 1))
-
-    create_list(:purchase_order, 16, :arrived,
-                status: 5,
-                season: 'SS14',
-                po_number: 234,
-                delivery_date: Time.new(2011, 1, 1))
-
-    create_list(:purchase_order, 15,
-                vendor: vendor,
-                status: 2,
-                season: 'SS15',
-                po_number: 345,
-                delivery_date: Time.new(2014, 1, 1))
+    create_purchase_orders
   end
 
   scenario 'Cancelling a purchase order line item' do
@@ -42,13 +25,13 @@ feature 'Cancelling purchase orders' do
     page.driver.post cancel_purchase_order_path(id: PurchaseOrder.first)
   end
 
-  def when_i_cancel_multiple_line_items
-    page.driver.post cancel_purchase_order_path(id: [PurchaseOrder.first, PurchaseOrder.second])
-  end
-
   def then_the_line_item_should_be_cancelled
     expect(PurchaseOrder.first).to be_cancelled
     expect(PurchaseOrder.second).to_not be_cancelled
+  end
+
+  def when_i_cancel_multiple_line_items
+    page.driver.post cancel_purchase_order_path(id: [PurchaseOrder.first, PurchaseOrder.second])
   end
 
   def then_the_line_items_should_be_cancelled
@@ -62,14 +45,43 @@ feature 'Cancelling purchase orders' do
   end
 
   def then_the_entire_purchase_order_should_be_cancelled
-    not_cancelled = PurchaseOrder.where.not(status: -1)
-                                 .where.not(po_number: PurchaseOrder.first.po_number)
+    assert_entire_po_order_to_be_cancelled
+    assert_every_other_po_not_cancelled
+  end
 
+  private
+
+  def create_purchase_orders
+    create_list(:purchase_order, 20,
+                status: 4,
+                season: 'AW15',
+                po_number: 123,
+                delivery_date: Time.new(2013, 1, 1))
+
+    create_list(:purchase_order, 16, :arrived,
+                status: 5,
+                season: 'SS14',
+                po_number: 234,
+                delivery_date: Time.new(2011, 1, 1))
+
+    create_list(:purchase_order, 15,
+                vendor: vendor,
+                status: 2,
+                season: 'SS15',
+                po_number: 345,
+                delivery_date: Time.new(2014, 1, 1))
+  end
+
+  def assert_entire_po_order_to_be_cancelled
     items = PurchaseOrder.where(po_number: PurchaseOrder.first.po_number)
     items.each do |item|
       expect(item).to be_cancelled
     end
+  end
 
+  def assert_every_other_po_not_cancelled
+    not_cancelled = PurchaseOrder.where.not(status: -1)
+                                 .where.not(po_number: PurchaseOrder.first.po_number)
     not_cancelled.each do |item|
       expect(item).to_not be_cancelled
     end
