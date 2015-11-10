b00d28e8d51f137d167fed50fcd2408053713310
Chris Blackburn <christopher.blackburn@gmail.com>
2015-11-10 16:50:58 +0000

    Correct field name for element ID

--- api/app/models/element.rb
+++ api/app/models/element.rb
@@ -6,6 +6,6 @@ class Element < ActiveRecord::Base
       .joins('LEFT JOIN ds_language_product_options ON
               mnp_elements.elementname = ds_language_product_options.pOption')
       .where('ds_language_product_options.oID = ?', option_id)
-      .where('ds_language_product_options.langID = 1').first.try(:element_id)
+      .where('ds_language_product_options.langID = 1').first.try(:elementid)
   end
 end
