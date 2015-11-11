22232907810b15384a95c0305fc775f0166eca2d
Luke Morton <lukemorton.dev@gmail.com>
2015-11-11 12:12:35 +0000

    Add link to edit

--- processing_frontend/app/views/orders/_table_row.jsx
+++ processing_frontend/app/views/orders/_table_row.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export class OrdersTableRow extends React.Component {
   render() {
     return (
       <tr>
-        <td>Shopping list {this.props.id}</td>
+        <td>
+          <Link to={`/orders/${this.props.id}/edit`}>
+            Shopping list {this.props.id}
+          </Link>
+        </td>
         <td className="text-center">{this.props.createdAt}</td>
         <td className="text-center">{this.props.exportedAt || '✘'}</td>
       </tr>
