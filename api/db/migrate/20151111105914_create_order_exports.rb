d96c5ba44bb4d0d039810353098cde6305f99edc
Luke Morton <lukemorton.dev@gmail.com>
2015-11-12 11:02:27 +0000

    Remove foreign key constraints from migrations

--- api/db/migrate/20151111105914_create_order_exports.rb
+++ api/db/migrate/20151111105914_create_order_exports.rb
@@ -1,8 +1,8 @@
 class CreateOrderExports < ActiveRecord::Migration
   def change
     create_table :order_exports do |t|
-      t.references :order, index: true, foreign_key: true
-      t.references :purchase_order, index: true, foreign_key: true
+      t.references :order, index: true
+      t.references :purchase_order, index: true
       t.timestamps
     end
   end
