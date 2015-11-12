3d9be158e9e46502e69382418db50e46da9fca51
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-12 11:20:11 +0000

    Add panels on edit page

--- processing_frontend/app/views/orders/edit.jsx
+++ processing_frontend/app/views/orders/edit.jsx
@@ -39,19 +39,24 @@ class OrdersEdit extends React.Component {
     return (
       <div className="row">
         <div className="col-md-12">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Internal SKU</th>
-                <th>Quantity</th>
-                <th>Cost</th>
-                <th>Discount</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.renderLineItems()}
-            </tbody>
-          </table>
+          <div className="panel panel-default">
+            <div className="panel-heading">Order line items</div>
+            <div className="panel-body">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Internal SKU</th>
+                    <th>Quantity</th>
+                    <th>Cost</th>
+                    <th>Discount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.renderLineItems()}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -65,55 +70,60 @@ class OrdersEdit extends React.Component {
     return (
       <div className="row">
         <div className="col-md-12">
-          <form className="form" onSubmit={this.handleLineItemSubmit.bind(this)}>
-            <div className="form-group col-md-2">
-              <label htmlFor="internalSku">Internal Sku</label>
-              <input type="text"
-                     name="internalSku"
-                     onChange={this.handleChange.bind(this, 'internalSku')}
-                     className="form-control"
-                     value={this.state.internalSku} />
+          <div className="panel panel-default">
+            <div className="panel-heading">Create line item</div>
+            <div className="panel-body">
+              <form className="form" onSubmit={this.handleLineItemSubmit.bind(this)}>
+                <div className="form-group col-md-2">
+                  <label htmlFor="internalSku">Internal Sku</label>
+                  <input type="text"
+                         name="internalSku"
+                         onChange={this.handleChange.bind(this, 'internalSku')}
+                         className="form-control"
+                         value={this.state.internalSku} />
+                </div>
+                <div className="form-group col-md-2">
+                  <label htmlFor="quantity">Quantity</label>
+                  <input type="number"
+                         name="quantity"
+                         onChange={this.handleChange.bind(this, 'quantity')}
+                         className="form-control"
+                         value={this.state.quantity} />
+                </div>
+                <div className="form-group col-md-2">
+                  <label htmlFor="cost">Cost</label>
+                  <input type="number"
+                         step="0.01"
+                         name="cost"
+                         onChange={this.handleChange.bind(this, 'cost')}
+                         className="form-control"
+                         value={this.state.cost} />
+                </div>
+                <div className="form-group col-md-2">
+                  <label htmlFor="discount">Discount</label>
+                  <input type="number"
+                         step="0.01"
+                         name="discount"
+                         onChange={this.handleChange.bind(this, 'discount')}
+                         className="form-control"
+                         value={this.state.discount} />
+                </div>
+                <div className="form-group col-md-2">
+                  <label htmlFor="dropDate">Drop Date</label>
+                  <input type="date"
+                         name="dropDate"
+                         onChange={this.handleChange.bind(this, 'dropDate')}
+                         className="form-control"
+                         value={this.state.dropDate} />
+                </div>
+                <div className="form-group col-md-2" style={{ marginTop: '1.7em' }}>
+                  <button className="btn btn-success">
+                    Create
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className="form-group col-md-2">
-              <label htmlFor="quantity">Quantity</label>
-              <input type="number"
-                     name="quantity"
-                     onChange={this.handleChange.bind(this, 'quantity')}
-                     className="form-control"
-                     value={this.state.quantity} />
-            </div>
-            <div className="form-group col-md-2">
-              <label htmlFor="cost">Cost</label>
-              <input type="number"
-                     step="0.01"
-                     name="cost"
-                     onChange={this.handleChange.bind(this, 'cost')}
-                     className="form-control"
-                     value={this.state.cost} />
-            </div>
-            <div className="form-group col-md-2">
-              <label htmlFor="discount">Discount</label>
-              <input type="number"
-                     step="0.01"
-                     name="discount"
-                     onChange={this.handleChange.bind(this, 'discount')}
-                     className="form-control"
-                     value={this.state.discount} />
-            </div>
-            <div className="form-group col-md-2">
-              <label htmlFor="dropDate">Drop Date</label>
-              <input type="date"
-                     name="dropDate"
-                     onChange={this.handleChange.bind(this, 'dropDate')}
-                     className="form-control"
-                     value={this.state.dropDate} />
-            </div>
-            <div className="form-group col-md-2" style={{ marginTop: '1.7em' }}>
-              <button className="btn btn-success">
-                Create
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     );
@@ -150,24 +160,29 @@ class OrdersEdit extends React.Component {
     return (
       <div className="row">
         <div className="col-md-12">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Status</th>
-                <th>Created</th>
-                <th>Updated</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{this.props.order.id}</td>
-                <td>{this.props.order.status}</td>
-                <td>{this.props.order.createdAt}</td>
-                <td>{this.props.order.updatedAt}</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="panel panel-default">
+            <div className="panel-heading">Order detail</div>
+            <div className="panel-body">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Status</th>
+                    <th>Created</th>
+                    <th>Updated</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{this.props.order.id}</td>
+                    <td>{this.props.order.status}</td>
+                    <td>{this.props.order.createdAt}</td>
+                    <td>{this.props.order.updatedAt}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     );
