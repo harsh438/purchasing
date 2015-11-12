3d83f7e20a4c6b8e73b85ef919546529c4e2d70d
Luke Morton <lukemorton.dev@gmail.com>
2015-11-12 16:36:09 +0000

    Add some defaults and legacy data to PurchaseOrder

--- api/app/models/purchase_order.rb
+++ api/app/models/purchase_order.rb
@@ -7,7 +7,21 @@ class PurchaseOrder < ActiveRecord::Base
   map_attributes id: :po_num,
                  order_type: :orderType
 
+
+  after_initialize :ensure_defaults
+  after_initialize :set_legacy
+
   def po_number
     id
+  end
+
+  private
+
+  def ensure_defaults
+    self.po_date ||= Time.now
+  end
+
+  def set_legacy
+    self.orderGrouping = ''
   end
 end
