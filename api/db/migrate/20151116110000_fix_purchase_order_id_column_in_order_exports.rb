506d89f5e8239e964b28040d7e228b9dcb44faf0
Chris Blackburn <christopher.blackburn@gmail.com>
2015-11-16 18:41:48 +0000

    Don't die if the foreign key doesn't exist

--- api/db/migrate/20151116110000_fix_purchase_order_id_column_in_order_exports.rb
+++ api/db/migrate/20151116110000_fix_purchase_order_id_column_in_order_exports.rb
@@ -1,5 +1,9 @@
 class FixPurchaseOrderIdColumnInOrderExports < ActiveRecord::Migration
   def change
-    remove_foreign_key :order_exports, { column: :purchase_order_id }
+    begin
+      remove_foreign_key :order_exports, { column: :purchase_order_id }
+    rescue ArgumentError => e
+      Rails.logger.info "Foreign key not found"
+    end
   end
 end
