fc02a271b6a95ee2f9ce4312c71670ca9de5d5f4
Chris Blackburn <christopher.blackburn@gmail.com>
2015-11-17 09:46:41 +0000

    Prefer single quotes

--- api/db/migrate/20151116110000_fix_purchase_order_id_column_in_order_exports.rb
+++ api/db/migrate/20151116110000_fix_purchase_order_id_column_in_order_exports.rb
@@ -3,7 +3,7 @@ class FixPurchaseOrderIdColumnInOrderExports < ActiveRecord::Migration
     begin
       remove_foreign_key :order_exports, { column: :purchase_order_id }
     rescue ArgumentError => e
-      Rails.logger.info "Foreign key not found"
+      Rails.logger.info 'Foreign key not found'
     end
   end
 end
