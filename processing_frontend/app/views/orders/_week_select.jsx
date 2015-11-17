ab8570ca783c6c7841954274a7719fca918a639c
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-17 12:18:39 +0000

    Fix weekselect

--- processing_frontend/app/views/orders/_week_select.jsx
+++ processing_frontend/app/views/orders/_week_select.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { map, range } from 'lodash';
 
+function getMonday(d) {
+  d = new Date(d);
+  let day = d.getDay(), diff = d.getDate() - day + (day == 0 ? -6:1);
+  return new Date(d.setDate(diff));
+}
+
 export function dropDates() {
   return map(range(52), (n) => {
     let date = new Date()
     date.setDate(date.getDate() + (n * 7))
-    date = this.getMonday(date);
+    date = getMonday(date);
     return { value: date.toISOString().slice(0,10), label: date.toDateString() }
   });
 }
 
-export default class WeekSelect extends React.Component {
+export class WeekSelect extends React.Component {
   componentWillMount() {
     this.props.table.setState({ dropDate: dropDates()[0].value })
   }
@@ -41,11 +47,5 @@ export default class WeekSelect extends React.Component {
 
   handleChange (field, { target }) {
     this.props.table.setState({ dropDate: this.refs.dropDate.value })
-  }
-
-  getMonday(d) {
-    d = new Date(d);
-    let day = d.getDay(), diff = d.getDate() - day + (day == 0 ? -6:1);
-    return new Date(d.setDate(diff));
   }
 }
