4761a10140c836597669d039ff799b420e5685ed
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-23 17:44:07 +0000

    Update sku model

--- api/app/models/sku.rb
+++ api/app/models/sku.rb
@@ -3,23 +3,7 @@ class Sku < ActiveRecord::Base
 
   belongs_to :product
   belongs_to :element
-  belongs_to :product_option
+  belongs_to :product_option, foreign_key: :option_id
 
-  validates_presence_of :element_id
   validates_presence_of :manufacturer_sku
-  validates_presence_of :product_id
-  validates_presence_of :option_id
-  validates_presence_of :season
-  validates_presence_of :sku
-
-  def self.generate_from!(attrs)
-    Sku.create(generate_attrs_from(attrs))
-  end
-
-  private
-
-  def self.generate_attrs_from(attrs)
-    attrs.merge!({ sku: "#{attrs[:product_id]}-#{attrs[:element_id]}",
-                   option_id: ProductOption.id_from_element(attrs[:product_id], attrs[:element_id]) })
-  end
 end
