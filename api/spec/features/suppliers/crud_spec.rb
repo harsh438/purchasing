9625c431690e5bede4ef7cab3cde19f24f76f3f5
Luke Morton <lukemorton.dev@gmail.com>
2015-11-23 16:34:19 +0000

    Add when/then steps

--- api/spec/features/suppliers/crud_spec.rb
+++ api/spec/features/suppliers/crud_spec.rb
@@ -2,13 +2,29 @@ feature 'Suppliers CRUD' do
   subject { JSON.parse(page.body) }
 
   scenario 'Creating a Supplier' do
-    page.driver.post suppliers_path(supplier: supplier_attrs)
-    expect(subject).to include(supplier_attrs)
+    when_i_create_a_supplier
+    then_i_should_be_provided_with_supplier_attributes
   end
 
   scenario 'Getting a Supplier' do
+    when_retrieving_supplier
+    then_i_should_be_provided_with_supplier_id_and_name
+  end
+
+  def when_i_create_a_supplier
+    page.driver.post suppliers_path(supplier: supplier_attrs)
+  end
+
+  def then_i_should_be_provided_with_supplier_attributes
+    expect(subject).to include(supplier_attrs)
+  end
+
+  def when_retrieving_supplier
     @supplier = create(:supplier)
     visit supplier_path(@supplier)
+  end
+
+  def then_i_should_be_provided_with_supplier_id_and_name
     expect(subject).to include("id" => @supplier.id,
                                "name" => @supplier.name)
   end
