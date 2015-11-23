7c0f745e323d61188d9da65449d163ff9fd53314
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-23 12:54:42 +0000

    Add id getter in product option

--- api/app/models/product_option.rb
+++ api/app/models/product_option.rb
@@ -2,11 +2,21 @@ class ProductOption < ActiveRecord::Base
   self.table_name = :ds_language_product_options
 
   def self.oid_from_element(product_id, element_id)
-    select('ds_language_product_options.oID')
+    select_from_element('ds_language_product_options.oID', product_id, element_id).try(:oID)
+  end
+
+  def self.id_from_element(product_id, element_id)
+    select_from_element('ds_language_product_options.id', product_id, element_id).try(:id)
+  end
+
+  private
+
+  def self.select_from_element(selection, product_id, element_id)
+    select(selection)
       .joins('LEFT JOIN mnp_elements ON
               mnp_elements.elementname = ds_language_product_options.pOption')
       .where('mnp_elements.elementid = ?', element_id)
       .where('ds_language_product_options.pID = ?', product_id)
-      .where('ds_language_product_options.langID = 1').first.try(:oID)
+      .where('ds_language_product_options.langID = 1').first
   end
 end
