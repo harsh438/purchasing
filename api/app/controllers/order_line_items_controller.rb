ea08b030d8b99770f9601b35acefe6265e5922cc
Luke Morton <lukemorton.dev@gmail.com>
2015-11-24 10:12:21 +0000

    Tidy up

--- api/app/controllers/order_line_items_controller.rb
+++ api/app/controllers/order_line_items_controller.rb
@@ -1,20 +1,22 @@
 class OrderLineItemsController < ApplicationController
+  def update
+    order_line_item.update!(line_item_attrs)
+    render json: order_line_item.order.as_json_with_line_items_and_purchase_orders
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages,
+                   fields: line_item_attrs.keys,
+                   ids: [order_line_item.id] }
+  end
+
   def destroy
     OrderLineItem.delete(params[:id])
     render json: [params[:id]]
   end
 
-  def update
-    order_line_item.update!(line_item_attrs)
-    render json: order_line_item.order.as_json_with_line_items_and_purchase_orders
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { errors: e.record.errors.full_messages, fields: line_item_attrs.keys, ids: [order_line_item.id] }
-  end
-
   private
 
   def line_item_attrs
-    params.require(:order_line_item).permit([:cost, :discount, :quantity, :drop_date])
+    params.require(:order_line_item).permit(:cost, :discount, :quantity, :drop_date)
   end
 
   def order_line_item
