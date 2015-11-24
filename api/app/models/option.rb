97ab8f7a9e19f4c2b9f4fa2d514c882926481fd1
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-24 15:54:14 +0000

    Add option model

---
+++ api/app/models/option.rb
@@ -0,0 +1,10 @@
+class Option < ActiveRecord::Base
+  self.table_name = :ds_options
+
+  include LegacyMappings
+
+  map_attributes id: :oID,
+                 product_id: :pID,
+                 parent_id: :parentID,
+                 name: :oNum
+end
