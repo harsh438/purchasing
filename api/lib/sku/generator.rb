ebbfc7813d50fe53e6d983599e42265e6acc9613
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-24 15:54:48 +0000

    Add sku, product and option creation to sku generator

--- api/lib/sku/generator.rb
+++ api/lib/sku/generator.rb
@@ -1,5 +1,40 @@
 class Sku::Generator
-  def self.sku_from!(attrs)
-    Sku.find_by(manufacturer_sku: attrs[:manufacturer_sku])
+  def sku_from!(attrs)
+    @attrs = attrs
+    sku = Sku.find_by(manufacturer_sku: attrs[:manufacturer_sku])
+
+    if sku.nil?
+      sku = generate_new_sku
+    end
+
+    sku
+  end
+
+  private
+
+  def generate_new_sku
+    product = Product.create(product_attrs)
+    option = Option.create(option_attrs(product))
+    Sku.create(sku_attrs(product, option))
+  end
+
+  def attrs
+    @attrs
+  end
+
+  def sku_attrs(product, option)
+    attrs.merge!({ product_id: product.id,
+                   option_id: option.id })
+  end
+
+  def product_attrs
+    { name: attrs[:manufacturer_sku],
+      price: attrs[:price],
+      on_sale: attrs[:on_sale] || '' }
+  end
+
+  def option_attrs(product)
+    { product_id: product.id,
+      name: attrs[:manufacturer_sku] }
   end
 end
