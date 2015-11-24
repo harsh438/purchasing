a61173f5f89db305e40726da28cca29ef0d2d961
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-24 16:03:45 +0000

    Fix spec

--- api/spec/unit/lib/sku/generator_spec.rb
+++ api/spec/unit/lib/sku/generator_spec.rb
@@ -1,7 +1,19 @@
 describe Sku::Generator do
-  context 'find a sku based on a previously existing sku' do
-    before { create(:sku) }
+  before { create(:sku) }
 
+  let(:new_sku_attrs) { { manufacturer_sku: 'MANU-FACTURER-SKU-12',
+                          manufacturer_color: 'blueish',
+                          manufacturer_size: 'smallish',
+                          season: 'witch',
+                          color: 'blue',
+                          size: 'small',
+                          color_family: 'blues',
+                          size_scale: 'thumb',
+                          cost_price: 10.00,
+                          list_price: 15.00,
+                          price: 5.00 } }
+
+  context 'find a sku based on a previously existing sku' do
     it 'should retrieve the exitsing sku' do
       sku = subject.sku_from!({ manufacturer_sku: 'MANU-FACTURER-SKU-11' })
 
@@ -16,21 +28,11 @@ describe Sku::Generator do
 
   context 'generate a sku based on the information passed in the attributes' do
     it 'should create a product and an option' do
-      sku = subject.sku_from!({ manufacturer_sku: 'MANU-FACTURER-SKU-12',
-                                manufacturer_color: 'blueish',
-                                manufacturer_size: 'smallish',
-                                season: 'witch',
-                                color: 'blue',
-                                size: 'small',
-                                color_family: 'blues',
-                                size_scale: 'thumb',
-                                cost_price: 10.00,
-                                list_price: 15.00,
-                                price: 5.00 })
+      sku = subject.sku_from!(new_sku_attrs)
 
       expect(sku).to_not be_nil
       expect(sku.product).to be_a(Product)
-      expect(sku.option).to be_a(Option)
+      expect(sku.product.price).to eq(new_sku_attrs[:price])
     end
   end
 end
