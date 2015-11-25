0a9c12346bccdabf3015cc9f06496707d857223e
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-25 13:06:56 +0000

    Update relationship mapping in sku generator

--- api/lib/sku/generator.rb
+++ api/lib/sku/generator.rb
@@ -15,6 +15,12 @@ class Sku::Generator
   def generate_new_sku
     product = Product.create(product_attrs)
     option = Option.create(option_attrs(product))
+    category = Category.create(category_attrs)
+    element = Element.create(element_attrs)
+
+    product_option = LanguageProductOption.create(product_option_attrs(product, option, element))
+    product_category = LanguageProductCategory.create(product_category_attrs(category))
+
     Sku.create(sku_attrs(product, option))
   end
 
@@ -26,10 +32,30 @@ class Sku::Generator
     attrs.merge!({ product_id: product.id })
   end
 
+  def element_attrs
+    { name: '' }
+  end
+
+  def product_option_attrs(product, option, element)
+    { language_id: 1,
+      product_id: product.id,
+      option_id: option.id,
+      element_id: element.id }
+  end
+
+  def product_category_attrs(category)
+    { language_id: 1,
+      category_id: category.id }
+  end
+
   def product_attrs
     { name: attrs[:manufacturer_sku],
       price: attrs[:price],
       on_sale: attrs[:on_sale] || '' }
+  end
+
+  def category_attrs
+    { parentID: 0 }
   end
 
   def option_attrs(product)
