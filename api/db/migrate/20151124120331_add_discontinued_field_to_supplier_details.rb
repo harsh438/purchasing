eaad4e9d9e0d6f52c6d2b2b9d9f0139f9417d274
Chris Blackburn <christopher.blackburn@gmail.com>
2015-11-25 14:32:41 +0000

    Temporarily disable migration

--- api/db/migrate/20151124120331_add_discontinued_field_to_supplier_details.rb
+++ api/db/migrate/20151124120331_add_discontinued_field_to_supplier_details.rb
@@ -1,6 +1,5 @@
 class AddDiscontinuedFieldToSupplierDetails < ActiveRecord::Migration
   def change
-    add_column :supplier_details, :discontinued, :boolean, default: false
-    add_index :supplier_details, :discontinued
+
   end
 end
