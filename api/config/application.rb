c80b9f6cd76b5304b815c0d20bb8e3905decf391
Luke Morton <lukemorton.dev@gmail.com>
2015-11-25 10:26:41 +0000

    Add paperclip

--- api/config/application.rb
+++ api/config/application.rb
@@ -14,5 +14,16 @@ module Purchasing
     config.http_auth = { enabled: true,
                          username: 'purchasing',
                          password: 'lastordersplease' }
+
+    config.s3 = { access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                  destination: { key: ENV['DESTINATION_BUCKET_KEY'],
+                                 region: ENV['DESTINATION_BUCKET_REGION'] } }
+
+    config.paperclip_defaults = { s3_permissions: :private,
+                                  s3_credentials: { bucket: ENV['DESTINATION_BUCKET_KEY'],
+                                                    s3_host_name: "s3-#{ENV['DESTINATION_BUCKET_REGION']}.amazonaws.com",
+                                                    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                                    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] } }
   end
 end
