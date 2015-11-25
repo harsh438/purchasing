4f6be0013df5b1dacd406ecffe21be43346fb0a4
Luke Morton <lukemorton.dev@gmail.com>
2015-11-25 10:48:24 +0000

    Remove s3 config since only need paperclip

--- api/config/application.rb
+++ api/config/application.rb
@@ -15,11 +15,6 @@ module Purchasing
                          username: 'purchasing',
                          password: 'lastordersplease' }
 
-    config.s3 = { access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                  destination: { key: ENV['DESTINATION_BUCKET_KEY'],
-                                 region: ENV['DESTINATION_BUCKET_REGION'] } }
-
     config.paperclip_defaults = { s3_permissions: :private,
                                   s3_credentials: { bucket: ENV['DESTINATION_BUCKET_KEY'],
                                                     s3_host_name: "s3-#{ENV['DESTINATION_BUCKET_REGION']}.amazonaws.com",
