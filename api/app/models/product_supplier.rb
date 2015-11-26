f7d2e443f317885f3845e2e5cbd1d23a96fc750a
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-26 12:37:53 +0000

    Add product_supplier table name

--- api/app/models/product_supplier.rb
+++ api/app/models/product_supplier.rb
@@ -1,5 +1,6 @@
 class ProductSupplier < ActiveRecord::Base
   include LegacyMappings
+  self.table_name = :product_supplier
 
   map_attributes product_id: :pid,
                  supplier_id: :supplierID
