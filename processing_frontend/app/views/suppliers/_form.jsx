85dfd0b42994212f04adbb8a23526f833587545d
MINETTE Alexandre <minette.alexandre@gmail.com>
2015-11-27 13:09:48 +0000

    reducing suppliers form vertical size

--- processing_frontend/app/views/suppliers/_form.jsx
+++ processing_frontend/app/views/suppliers/_form.jsx
@@ -9,7 +9,7 @@ export default class SuppliersForm extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     this.setState({ submitting: false });
-    
+
   	if (nextProps.supplier) {
 	  	this.setState(nextProps.supplier);
 	  }
@@ -25,116 +25,122 @@ export default class SuppliersForm extends React.Component {
             <form className="form"
                   onChange={this.handleFormChange.bind(this)}
                   onSubmit={this.handleFormSubmit.bind(this)}>
-              <div className="form-group">
-                <label forHtml="supplier_name">Name</label>
-                <input className="form-control"
-                       id="supplier_name"
-                       name="name"
-                       placeholder="Name"
-                       required
-                       value={this.state.name} />
-              </div>
-              <div className="form-group">
-                <label forHtml="return_address_name">Address Name</label>
-                <input className="form-control"
-                       id="returns_address_name"
-                       name="returns_address_name"
-                       placeholder="Return Address Name"
-                       required
-                       value={this.state.returnsAddressName} />
-              </div>
-              <div className="form-group">
-                <label forHtml="return_address_name">Address Number</label>
-                <input className="form-control"
-                       id="returns_address_number"
-                       name="returns_address_number"
-                       placeholder="Return Address Number"
-                       value={this.state.returnsAddressNumber} />
-              </div>
-              <div className="form-group">
-                <label forHtml="return_address_1">Returns Address Line 1</label>
-                <input className="form-control"
-                       id="returns_address_1"
-                       name="returns_address_1"
-                       placeholder="Line 1"
-                       value={this.state.returnsAddress1} />
-              </div>
-              <div className="form-group">
-                <label forHtml="returns_address_2">Returns Address Line 2</label>
-                <input className="form-control"
-                       id="returns_address_2"
-                       name="returns_address_2"
-                       placeholder="Line 2"
-                       value={this.state.returnsAddress2} />
-              </div>
-              <div className="form-group">
-                <label forHtml="returns_address_3">Returns Address Line 3</label>
-                <input className="form-control"
-                       id="returns_address_3"
-                       name="returns_address_3"
-                       placeholder="Line 3"
-                       value={this.state.returnsAddress3} />
-              </div>
-              <div className="form-group">
-                <label forHtml="return_address_name">Returns Postal Code</label>
-                <input className="form-control"
-                       id="returns_postal_code"
-                       name="returns_postal_code"
-                       placeholder="Postal Code"
-                       value={this.state.returnsPostalCode} />
-              </div>
-              <div className="form-group">
-                <label forHtml="returns_process">Return Process</label>
-                <input className="form-control"
-                       id="returns_process"
-                       name="returns_process"
-                       placeholder="Return Process"
-                       value={this.state.returnsProcess} />
-              </div>
-              <div className="form-group">
-                <label forHtml="invoicer_name">Invoicer Name</label>
-                <input className="form-control"
-                       id="invoicer_name"
-                       name="invoicer_name"
-                       placeholder="Invoicer Name"
-                       value={this.state.invoicerName} />
-              </div>
-              <div className="form-group">
-                <label forHtml="account_number">Account Number</label>
-                <input className="form-control"
-                       id="account_number"
-                       name="account_number"
-                       placeholder="Account Number"
-                       value={this.state.accountNumber} />
-              </div>
-              <div className="form-group">
-                <label forHtml="country_of_origin">Country Of Origin</label>
-                <input className="form-control"
-                       id="country_of_origin"
-                       name="country_of_origin"
-                       placeholder="Country Of Origin"
-                       value={this.state.countryOfOrigin} />
-              </div>
-              <div className="form-group">
-                <div className="checkbox">
-                  <label>
-                    <input type="checkbox"
-                           name="discontinued"
-                           value="1"
-                           className="checkbox"
-                           checked={this.state.discontinued}
-                           onChange={this.handleCheckboxChange.bind(this)} />
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <td><label forHtml="supplier_name">Name</label></td>
+                    <td><input className="form-control"
+                           id="supplier_name"
+                           name="name"
+                           placeholder="Name"
+                           required
+                           value={this.state.name} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="return_address_name">Address Name</label></td>
+                      <td><input className="form-control"
+                             id="returns_address_name"
+                             name="returns_address_name"
+                             placeholder="Return Address Name"
+                             required
+                             value={this.state.returnsAddressName} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="return_address_name">Address Number</label></td>
+                      <td><input className="form-control"
+                             id="returns_address_number"
+                             name="returns_address_number"
+                             placeholder="Return Address Number"
+                             value={this.state.returnsAddressNumber} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="return_address_1">Returns Address Line 1</label></td>
+                      <td><input className="form-control"
+                             id="returns_address_1"
+                             name="returns_address_1"
+                             placeholder="Line 1"
+                             value={this.state.returnsAddress1} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="returns_address_2">Returns Address Line 2</label></td>
+                      <td><input className="form-control"
+                             id="returns_address_2"
+                             name="returns_address_2"
+                             placeholder="Line 2"
+                             value={this.state.returnsAddress2} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="returns_address_3">Returns Address Line 3</label></td>
+                      <td><input className="form-control"
+                             id="returns_address_3"
+                             name="returns_address_3"
+                             placeholder="Line 3"
+                             value={this.state.returnsAddress3} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="return_address_name">Returns Postal Code</label></td>
+                      <td><input className="form-control"
+                             id="returns_postal_code"
+                             name="returns_postal_code"
+                             placeholder="Postal Code"
+                             value={this.state.returnsPostalCode} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="returns_process">Return Process</label></td>
+                      <td><input className="form-control"
+                             id="returns_process"
+                             name="returns_process"
+                             placeholder="Return Process"
+                             value={this.state.returnsProcess} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="invoicer_name">Invoicer Name</label></td>
+                      <td><input className="form-control"
+                             id="invoicer_name"
+                             name="invoicer_name"
+                             placeholder="Invoicer Name"
+                             value={this.state.invoicerName} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="account_number">Account Number</label></td>
+                      <td><input className="form-control"
+                             id="account_number"
+                             name="account_number"
+                             placeholder="Account Number"
+                             value={this.state.accountNumber} /></td>
+                  </tr>
+                  <tr>
+                      <td><label forHtml="country_of_origin">Country Of Origin</label></td>
+                      <td><input className="form-control"
+                             id="country_of_origin"
+                             name="country_of_origin"
+                             placeholder="Country Of Origin"
+                             value={this.state.countryOfOrigin} /></td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div className="checkbox">
+                        <label>
+                          <input type="checkbox"
+                                 name="discontinued"
+                                 value="1"
+                                 className="checkbox"
+                                 checked={this.state.discontinued}
+                                 onChange={this.handleCheckboxChange.bind(this)} />
 
-                    Discontinued
-                  </label>
-                </div>
-              </div>
-              <div className="form-group">
-                <button className="btn btn-success col-xs-offset-3 col-xs-6"
-                        disabled={this.state.submitting}>
-                  {this.props.submitText}
-                </button>
-              </div>
+                          Discontinued
+                        </label>
+                      </div>
+                      </td><td></td>
+                  </tr>
+                  <tr>
+                    <td></td><td><button className="btn btn-success col-xs-6"
+                            disabled={this.state.submitting}>
+                      {this.props.submitText}
+                    </button></td>
+                  </tr>
+                </tbody>
+              </table>
             </form>
           </div>
         </div>
