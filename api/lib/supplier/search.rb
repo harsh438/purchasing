576770337fc15f7b6da85b53f264e24ec648068e
Luke Morton <lukemorton.dev@gmail.com>
2015-11-27 15:48:18 +0000

    Fix n+1

--- api/lib/supplier/search.rb
+++ api/lib/supplier/search.rb
@@ -1,6 +1,6 @@
 class Supplier::Search
   def search(params)
-    suppliers = Supplier.latest.with_details.with_contacts
+    suppliers = Supplier.latest.includes(:details, :contacts, :terms)
     suppliers = apply_filters(suppliers, params[:filters] || {})
     suppliers = suppliers.page(params[:page])
     suppliers
