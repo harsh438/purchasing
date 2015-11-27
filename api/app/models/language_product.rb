885b6358a2a7afbd5ee9a3c65b7c7274afeb4b36
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-27 11:05:49 +0000

    Add language product legacy mappings

--- api/app/models/language_product.rb
+++ api/app/models/language_product.rb
@@ -1,3 +1,13 @@
 class LanguageProduct < ActiveRecord::Base
   self.table_name = :ds_language_products
+
+  include LegacyMappings
+
+  map_attributes id: :lpID,
+                 product_id: :pID,
+                 language_id: :langID,
+                 name: :pName,
+                 teaser: :pTeaser,
+                 description: :pDesc,
+                 email_display: :pCallEmailDisplay
 end
