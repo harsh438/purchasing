927ce23b8e4d77bff6a94aee686943eaac68301b
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-11-30 17:26:18 +0000

    Add system to call our service from PHP, addable into existing createPID script

---
+++ php/create_pid.php
@@ -0,0 +1,26 @@
+<?php
+  $endpoint = 'http://localhost:5000/api/skus.json';
+  $ch = curl_init();
+
+  $opts = array('manufacturer_sku' => 'MANU-FACTURER-SKU-12',
+                'manufacturer_color' => 'blueish',
+                'manufacturer_size' => 'smallish',
+                'season' => 'witch',
+                'color' => 'blue',
+                'size' => 'small',
+                'color_family' => 'blues',
+                'size_scale' => 'thumb',
+                'cost_price' => 10.00,
+                'list_price' => 15.00,
+                'price' => 5.00);
+
+  curl_setopt($ch, CURLOPT_URL, $endpoint);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($opts));
+  $output = curl_exec($ch);
+
+  var_dump($output);
+
+  curl_close($ch);
+?>
