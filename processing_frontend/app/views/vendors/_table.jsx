cb493f4497874410fbe6c54ad83f7fb000d6c1ae
Luke Morton <lukemorton.dev@gmail.com>
2015-12-02 15:13:34 +0000

    Tidy up columns

--- processing_frontend/app/views/vendors/_table.jsx
+++ processing_frontend/app/views/vendors/_table.jsx
@@ -3,30 +3,30 @@ import { map } from 'lodash';
 import { Link } from 'react-router';
 
 export default class VendorsTable extends React.Component {
-	render() {
-  	return (
-			<table className="table">
-				<thead>
-					<tr>
-						<th>Name</th>
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
             <th>Suppliers</th>
-            <th className="text-center">Created</th>
-            <th className="text-center">Updated</th>
-            <th className="text-center">Discontinued</th>
-					</tr>
-				</thead>
-				<tbody>
-					{this.renderRows()}
-				</tbody>
-			</table>
-  	);
+            <th className="text-center" style={{ width: '15%' }}>Created</th>
+            <th className="text-center" style={{ width: '15%' }}>Updated</th>
+            <th className="text-center" style={{ width: '15%' }}>Discontinued</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderRows()}
+        </tbody>
+      </table>
+    );
   }
 
   renderRows() {
     return map(this.props.vendors, (vendor, i) => {
       return (
         <tr key={i}>
-        	<td>
+          <td>
             <Link to={`/vendors/${vendor.id}/edit`}>
               {vendor.name}
             </Link>
@@ -46,7 +46,7 @@ export default class VendorsTable extends React.Component {
     });
   }
 
-	supplierNames(suppliers) {
-		return map(suppliers, supplier => supplier.name).join(', ');
-	}
+  supplierNames(suppliers) {
+    return map(suppliers, supplier => supplier.name).join(', ');
+  }
 }
