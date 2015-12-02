e990bb8a94d5226d7fa27f7a5db43bc60b0124f9
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-02 16:43:35 +0000

    Add and use real endpoint for pvx

--- api/spec/unit/lib/sku/api_spec.rb
+++ api/spec/unit/lib/sku/api_spec.rb
@@ -1,11 +1,13 @@
 describe Sku::Api do
+  let (:url) { Sku::Api.new.config['url'] }
+
   before(:each) do
-    stub_request(:get, Regexp.new(Sku::Api.new.url + '\?.*'))
+    stub_request(:post, Regexp.new(url))
       .to_return(status: 200, body: { m_sku: '12345',
                                       size: 'Small',
                                       extra_detail: 'Cool' }.to_json)
 
-    stub_request(:get, Regexp.new(Sku::Api.new.url + '/timeout')).to_timeout
+    stub_request(:post, Regexp.new(url + '/timeout')).to_timeout
   end
 
   context 'Retrieving sku information from the API' do
@@ -19,7 +21,7 @@ describe Sku::Api do
   context 'Handling API timeouts' do
     it 'should explode' do
       expect { subject.find({ m_sku: '12345', size: 'Small' },
-                            Sku::Api.new.url + '/timeout') }.to raise_error(Excon::Errors::Timeout)
+                            url + '/timeout') }.to raise_error(Excon::Errors::Timeout)
     end
   end
 end
