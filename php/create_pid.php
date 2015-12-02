711fbfcc0cb0ca446f601464dd47aadd851ec2c7
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-02 15:23:19 +0000

    Use tonys code for the create_pid php example

--- php/create_pid.php
+++ php/create_pid.php
@@ -1,25 +1,27 @@
 <?php
+$opts = array( 'manufacturer_sku' => 261040,
+                 'manufacturer_color' => 'Pale Blue',
+                 'manufacturer_size' => 'LRG',
+                 'season' => 'ss15',
+                 'color' => 'Blue',
+                 'size' => '06-9 mths',
+                 'cost_price' => 12.06,
+                 'price' => 18.00,
+                 'lead_gender' => 'm',
+                 'product_name' => 'Clarks Originals Boots - Clarks Originals Baby Warm  - Pale Blue',
+                 'vendor_id' => 919);
+
   $endpoint = 'https://purchasing.surfdome.cc/api/skus.json';
-  $ch = curl_init();
 
-  $opts = array('manufacturer_sku' => 'MANU-FACTURER-SKU-14',
-                'product_name' => 'A nice little shirt',
-                'lead_gender' => 'M',
-                'vendor_id' => 1,
-                'manufacturer_color' => 'blueish',
-                'manufacturer_size' => 'smallish',
-                'season' => 'witch',
-                'color' => 'blue',
-                'size' => 'small',
-                'color_family' => 'blues',
-                'cost_price' => 10.00,
-                'price' => 5.00);
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $endpoint);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_USERPWD, 'purchasing' . ":" . 'lastordersplease');
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_VERBOSE, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($opts));
+                $output = curl_exec($ch);
 
-  curl_setopt($ch, CURLOPT_URL, $endpoint);
-  curl_setopt($ch, CURLOPT_USERPWD, 'purchasing:lastordersplease');
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($opts));
   $output = curl_exec($ch);
 
   var_dump($output);
