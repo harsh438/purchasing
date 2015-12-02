f62dfe0e776b29d71f1037eb07bccd9131be6a74
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-02 17:26:08 +0000

    Add proper spec for Pvx api

--- api/spec/unit/lib/sku/api_spec.rb
+++ api/spec/unit/lib/sku/api_spec.rb
@@ -2,26 +2,28 @@ describe Sku::Api do
   let (:url) { Sku::Api.new.config['url'] }
 
   before(:each) do
-    stub_request(:post, Regexp.new(url))
-      .to_return(status: 200, body: { m_sku: '12345',
-                                      size: 'Small',
-                                      extra_detail: 'Cool' }.to_json)
 
-    stub_request(:post, Regexp.new(url + '/timeout')).to_timeout
   end
 
   context 'Retrieving sku information from the API' do
-    let(:response) { subject.find({ m_sku: '12345', size: 'Small' }) }
+    let(:response) do
+      VCR.use_cassette 'good_sku' do
+        subject.find({ man_sku: 'SV507-A59', size: '14' })
+      end
+    end
+
+    let(:nonexistent_response) do
+      VCR.use_cassette 'nonexistent_sku' do
+        subject.find({ man_sku: '12345', size: 'Small' })
+      end
+    end
 
     it 'should find a sku in the sku API and return previously unknown information on it' do
-      expect(JSON.parse(response.body)['extra_detail']).to eq('Cool')
+      expect(JSON.parse(response.body)['barcode']).to include('5052094029950')
     end
-  end
 
-  context 'Handling API timeouts' do
-    it 'should explode' do
-      expect { subject.find({ m_sku: '12345', size: 'Small' },
-                            url + '/timeout') }.to raise_error(Excon::Errors::Timeout)
+    it 'should return a 204 for nonexistent skus' do
+      expect(nonexistent_response.headers['status']).to include('204')
     end
   end
 end
