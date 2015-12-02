f619e5c34ce7dfc5ccfa590747962cc29bf5d8a4
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-02 15:22:59 +0000

    Add spec to check that the sku api explodes on request timeout

--- api/lib/sku/api.rb
+++ api/lib/sku/api.rb
@@ -1,10 +1,10 @@
 class Sku::Api
   def url
-    'http://www.example.com/'
+    'http://www.example.com/api'
   end
 
-  def find(fields)
-    connection = Excon.new(url)
+  def find(fields, custom_url = nil)
+    connection = Excon.new(custom_url || url)
     connection.get(query: fields)
   end
 end
--- api/spec/unit/lib/sku/api_spec.rb
+++ api/spec/unit/lib/sku/api_spec.rb
@@ -1,16 +1,25 @@
 describe Sku::Api do
   before(:each) do
-    stub_request(:get, Regexp.new(Sku::Api.new.url + '.*'))
+    stub_request(:get, Regexp.new(Sku::Api.new.url + '\?.*'))
       .to_return(status: 200, body: { m_sku: '12345',
                                       size: 'Small',
                                       extra_detail: 'Cool' }.to_json)
+
+    stub_request(:get, Regexp.new(Sku::Api.new.url + '/timeout')).to_timeout
   end
 
   context 'Retrieving sku information from the API' do
-    let(:response) { subject.find(m_sku: '12345', size: 'Small') }
+    let(:response) { subject.find({ m_sku: '12345', size: 'Small' }) }
 
     it 'should find a sku in the sku API and return previously unknown information on it' do
       expect(JSON.parse(response.body)['extra_detail']).to eq('Cool')
+    end
+  end
+
+  context 'Handling API timeouts' do
+    it 'should explode' do
+      expect { subject.find({ m_sku: '12345', size: 'Small' },
+                            Sku::Api.new.url + '/timeout') }.to raise_error(Excon::Errors::Timeout)
     end
   end
 end
