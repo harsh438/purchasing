ee5a088a0845516726b0c7e44c8ebf581f57414d
Chris Blackburn <christopher.blackburn@gmail.com>
2015-12-02 12:08:55 +0000

    New schema following migration run

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202105914) do
+ActiveRecord::Schema.define(version: 20151202115621) do
 
   create_table "ds_categories", primary_key: "catID", force: :cascade do |t|
     t.integer "parentID",         limit: 4,     default: 0,      null: false
