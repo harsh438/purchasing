a63eb7328dc5b73b2f084b50a2a3f442a49b4809
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-03 12:20:53 +0000

    Add api response class

---
+++ api/lib/sku/api_response.rb
@@ -0,0 +1,21 @@
+class Sku::ApiResponse
+  def initialize(response)
+    @response = response
+  end
+
+  def status
+    @response['headers']['Status'].split(' ').first.to_i
+  end
+
+  def fields
+    return {} if status != 200
+    map_to_response(@response['body'])
+  end
+
+  private
+
+  def map_to_response(fields)
+    { sku: fields['sku'],
+      barcode: fields['barcode'] }
+  end
+end
