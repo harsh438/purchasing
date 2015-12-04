293e322d4e3cf199ee4bdb568ef2d07fe3cb77b3
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-04 15:15:21 +0000

    Add language products to product factory

--- api/spec/factories/product_factory.rb
+++ api/spec/factories/product_factory.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
   factory :product do
-
-    # required defaults
     pSale 0
 
     vendor_id { create(:vendor).id }
+
+    after(:create) do |product, evaluator|
+      create(:language_product, product_id: product.id)
+    end
   end
 end
