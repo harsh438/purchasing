ab33f2ffa2dc053d1a4a12ff349ba274b3ee06b8
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-04 14:01:48 +0000

    Update schema

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204110107) do
+ActiveRecord::Schema.define(version: 20151204131713) do
 
   create_table "barcodes", force: :cascade do |t|
     t.integer "sku_id",  limit: 4
