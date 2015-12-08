780aeb3e6b7c049252763d475655d2e3570bd2d5
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-08 11:04:06 +0000

    Add additional params to skus controller

--- api/app/controllers/skus_controller.rb
+++ api/app/controllers/skus_controller.rb
@@ -24,20 +24,28 @@ class SkusController < ApplicationController
   end
 
   def sku_attrs
-    params.permit([:manufacturer_sku,
-                   :lead_gender,
-                   :vendor_id,
-                   :product_name,
-                   :manufacturer_color,
-                   :manufacturer_size,
-                   :season,
-                   :color,
-                   :size,
-                   :color_family,
-                   :size_scale,
-                   :cost_price,
-                   :list_price,
-                   :price,
-                   barcodes_attributes: [:barcode]])
+    params
+      .require(:sku)
+      .permit(:sku,
+              :product_id,
+              :language_product_id,
+              :element_id,
+              :option_id,
+              :category_id,
+              :manufacturer_sku,
+              :lead_gender,
+              :vendor_id,
+              :product_name,
+              :manufacturer_color,
+              :manufacturer_size,
+              :season,
+              :color,
+              :size,
+              :color_family,
+              :size_scale,
+              :cost_price,
+              :list_price,
+              :price,
+              barcodes_attributes: [:barcode])
   end
 end
