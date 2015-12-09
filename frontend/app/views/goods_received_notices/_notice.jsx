83a566ffbfe4006cd3a2d0f0a3576a249d029024
Luke Morton <lukemorton.dev@gmail.com>
2015-12-09 21:55:00 +0000

    Add styles for late and delivered

--- frontend/app/views/goods_received_notices/_notice.jsx
+++ frontend/app/views/goods_received_notices/_notice.jsx
@@ -3,18 +3,30 @@ import React from 'react';
 export default class GoodsReceivedNoticesNotice extends React.Component {
   render() {
     return (
-      <a href="#edit" className={`grn_notice list-group-item ${this.statusClass()}`}>
+      <a href="#edit" className={this.groupItemClass()}>
         GRN #{this.props.id}
 
-        <span className="badge grn_notice__badge" title="Pallets">{this.props.pallets}</span>
-        <span className="badge grn_notice__badge" title="Cartons">{this.props.cartons}</span>
-        <span className="badge grn_notice__badge" title="Units">{this.props.units}</span>
+        <span className={this.badgeClass()} title="Pallets">{this.props.pallets}</span>
+        <span className={this.badgeClass()} title="Cartons">{this.props.cartons}</span>
+        <span className={this.badgeClass()} title="Units">{this.props.units}</span>
       </a>
     );
   }
 
-  statusClass() {
+  groupItemClass() {
+    return `grn_notice list-group-item ${this.statusClass()}`;
+  }
+
+  badgeClass() {
+    return "grn_notice__badge badge";
+  }
+
+  statusClass(prefix) {
     switch (this.props.status) {
+    case 'late':
+      return 'grn_notice--late list-group-item-danger';
+    case 'delivered':
+      return 'grn_notice--delivered';
     default:
       return '';
     }
