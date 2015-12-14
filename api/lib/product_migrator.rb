5526d7e9c3456668e72617e3be39a7e84ccf75f0
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-14 10:25:15 +0000

    make migrator handle products with no options

--- api/lib/product_migrator.rb
+++ api/lib/product_migrator.rb
@@ -7,6 +7,18 @@ class ProductMigrator
 
   def migrate_single(product)
     @product = product
+    if product.language_product_options.count > 0
+      migrate_product_with_options
+    else
+      migrate_product_with_no_options
+    end
+
+    puts "Migrated product #{product.id}."
+  end
+
+  private
+
+  def migrate_product_with_options
     product.language_product_options.where(language_id: 1).each do |language_option|
       @language_option = language_option
 
@@ -16,11 +28,17 @@ class ProductMigrator
         puts validation_error
       end
     end
-
-    puts "Migrated product #{product.id}."
   end
 
-  private
+  def migrate_product_with_no_options
+    @language_option = nil
+
+    if ok_to_migrate?
+      Sku.create!(sku_attrs_with_no_option)
+    else
+      puts validation_error
+    end
+  end
 
   def product
     @product
@@ -72,6 +90,13 @@ class ProductMigrator
       .merge!(language_option_attrs)
   end
 
+  def sku_attrs_with_no_option
+    { sku: internal_sku }
+      .merge!(product_attrs)
+      .merge!(product_detail_attrs)
+      .merge!(language_product_attrs)
+  end
+
   def language_option_attrs
     { option_id: language_option.option_id,
       element_id: element.id }
@@ -86,7 +111,7 @@ class ProductMigrator
     { product_id: product.id,
       manufacturer_sku: product.manufacturer_sku,
       manufacturer_size: language_option.try(:option).try(:size),
-      size: language_option.name,
+      size: language_option.try(:name) || '',
       cost_price: product.cost,
       price: product.price,
       vendor_id: product.vendor_id,
