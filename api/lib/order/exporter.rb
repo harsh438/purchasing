2779fc2edf4281cf4bc9c319fc88e13309244891
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-16 15:45:04 +0000

    Fix ABC complexity

--- api/lib/order/exporter.rb
+++ api/lib/order/exporter.rb
@@ -24,24 +24,36 @@ class Order::Exporter
   end
 
   def create_po_line_item(order_line_item, extra_params)
-    PurchaseOrderLineItem.create!(supplier_list_price: order_line_item.product.cost,
-                                  product_rrp: order_line_item.product.price,
-                                  product_sku: order_line_item.product.manufacturer_sku,
-                                  product_size: order_line_item.product.size,
-                                  order_date: order_line_item.order.created_at,
-                                  drop_date: order_line_item.drop_date,
-                                  season: order_line_item.season || '',
-                                  gender: order_line_item.gender || '',
-                                  status: 2,
-                                  operator: extra_params[:operator] || 'REORDER_TOOL',
-                                  cost: order_line_item.discounted_cost,
-                                  vendor_id: order_line_item.vendor_id,
-                                  product_id: order_line_item.product_id,
-                                  option_id: order_line_item.option_id || 0,
-                                  quantity: order_line_item.quantity,
-                                  product_name: order_line_item.product_name,
-                                  reporting_pid: order_line_item.reporting_pid,
-                                  single_line_id: extra_params[:single_line_id] || nil)
+    PurchaseOrderLineItem.create!(po_line_item_attrs(order_line_item, extra_params))
+  end
+
+  def po_line_item_attrs(order_line_item, extra_params)
+    { supplier_list_price: order_line_item.product.cost,
+      order_date: order_line_item.order.created_at,
+      drop_date: order_line_item.drop_date,
+      season: order_line_item.season || '',
+      gender: order_line_item.gender || '',
+      status: 2,
+      operator: extra_params[:operator] || 'REORDER_TOOL',
+      cost: order_line_item.discounted_cost,
+      quantity: order_line_item.quantity,
+      reporting_pid: order_line_item.reporting_pid,
+      single_line_id: extra_params[:single_line_id] || nil }
+      .merge!(po_line_item_relationship_attrs(order_line_item))
+      .merge!(po_line_product_attrs(order_line_item))
+  end
+
+  def po_line_item_relationship_attrs(order_line_item)
+    { product_id: order_line_item.product_id,
+      vendor_id: order_line_item.vendor_id,
+      option_id: order_line_item.option_id || 0 }
+  end
+
+  def po_line_product_attrs(order_line_item)
+    { product_rrp: order_line_item.product.price,
+      product_sku: order_line_item.product.manufacturer_sku,
+      product_size: order_line_item.product.size,
+      product_name: order_line_item.product_name }
   end
 
   def assign_po_to_orders(purchase_order, orders)
