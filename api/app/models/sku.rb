65c09f25e583e1dc4bd724ec539946bab5dba996
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-12-21 17:19:57 +0000

    Update sku model with language_product_option id

--- api/app/models/sku.rb
+++ api/app/models/sku.rb
@@ -6,7 +6,8 @@ class Sku < ActiveRecord::Base
   belongs_to :element
   belongs_to :language_product, foreign_key: :language_product_id
   belongs_to :language_category, foreign_key: :category_id
-  belongs_to :language_product_option, foreign_key: :option_id
+  belongs_to :option, foreign_key: :option_id
+  belongs_to :language_product_option, foreign_key: :language_product_option_id
 
   has_many :barcodes
   accepts_nested_attributes_for :barcodes
