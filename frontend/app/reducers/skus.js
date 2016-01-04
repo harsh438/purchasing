0a63ad3d37d0c37a40f4572a65ce0969783fbacb
Luke <lukemorton.dev@gmail.com>
2016-01-04 13:47:20 +0000

    Remove debugging

--- frontend/app/reducers/skus.js
+++ frontend/app/reducers/skus.js
@@ -8,7 +8,6 @@ const initialState = { skus: [],
 export default function reduceSkus(state = initialState, action) {
   switch (action.type) {
   case 'LOAD_SKUS':
-    console.log(action.results);
     return assign({}, state, { skus: map(action.results.skus, camelizeKeys),
                                activePage: action.results.active_page,
                                totalPages: action.results.total_pages });
