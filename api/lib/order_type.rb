deb240fb58246fe3a79e27fcd0174218d6443b4c
Luke <lukemorton.dev@gmail.com>
2016-01-04 12:11:08 +0000

    Improve whitespace

--- api/lib/order_type.rb
+++ api/lib/order_type.rb
@@ -1,16 +1,16 @@
 class OrderType
   def self.string_from(char)
     case char.capitalize
-      when 'P'
-        'preorder'
-      when 'SR'
-        'suppliers_risk'
-      when 'R'
-        'reorder'
-      when 'O'
-        'over_delivery'
-      when 'B'
-        'bulk_order'
+    when 'P'
+      'preorder'
+    when 'SR'
+      'suppliers_risk'
+    when 'R'
+      'reorder'
+    when 'O'
+      'over_delivery'
+    when 'B'
+      'bulk_order'
     end
   end
 
@@ -20,16 +20,16 @@ class OrderType
 
   def self.char_from(str)
     case str.downcase
-      when 'preorder'
-        'P'
-      when 'suppliers_risk'
-        'SR'
-      when 'reorder'
-        'R'
-      when 'over_delivery'
-        'O'
-      when 'bulk_order'
-        'B'
+    when 'preorder'
+      'P'
+    when 'suppliers_risk'
+      'SR'
+    when 'reorder'
+      'R'
+    when 'over_delivery'
+      'O'
+    when 'bulk_order'
+      'B'
     end
   end
 end
