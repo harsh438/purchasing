9f8e9484465b09036fc69cb9549c780270d7bc40
Fareed Dudhia <fareeddudhia@googlemail.com>
2016-01-05 16:31:40 +0000

    Add real content to sku generation feature

--- api/spec/features/skus/generation_spec.rb
+++ api/spec/features/skus/generation_spec.rb
@@ -1,29 +1,44 @@
 feature 'SKU generation' do
-  subject { JSON.parse(page.body) }
+  subject { JSON.parse(page.body).with_indifferent_access }
 
-  scenario 'Generating skus from a product with multiple sizes' do
-    when_i_generate_a_sku_from_a_multi_sized_product
-    then_each_corresponding_option_should_create_a_sku
+  let (:multi_size_attrs) { { manufacturer_sku: 'DA-ADFADET-WHT',
+                              manufacturer_color: 'Pale Blue',
+                              manufacturer_size: '12',
+                              season: 'ss15',
+                              color: 'Blue',
+                              size: '06-9 mths',
+                              cost_price: 12.06,
+                              price: 18.00,
+                              lead_gender: 'M',
+                              product_name: 'Clarks Originals Boots - Clarks Originals Baby Warm - Pale Blue',
+                              vendor_id: 919,
+                              category_id: 12,
+                              category_name: 'Whatever',
+                              inv_track: 'P',
+                              barcode: '12389123' } }
+
+  scenario 'Generating skus using the API' do
+    when_i_generate_skus
+    then_correct_skus_should_be_generated
   end
 
-  scenario 'Generating a sku from a product with one size' do
-    when_i_generate_a_sku_from_a_single_sized_product
-    then_the_generated_sku_should_be_valid
+  def when_i_generate_skus
+    page.driver.post skus_path(multi_size_attrs)
   end
 
-  def when_i_generate_a_sku_from_a_multi_sized_product
+  def then_correct_skus_should_be_generated
+    multi_size_attrs.except(:cost_price,
+                            :price,
+                            :lead_gender,
+                            :category_id,
+                            :category_name,
+                            :inv_track,
+                            :barcode).each do |key, a|
+      expect(subject[key]).to eq(a)
+    end
 
-  end
-
-  def then_each_corresponding_option_should_create_a_sku
-
-  end
-
-  def when_i_generate_a_sku_from_a_single_sized_product
-
-  end
-
-  def then_the_generated_sku_should_be_valid
-
+    expect(subject[:sku]).to eq("#{Product.first.id}-#{Element.first.id}")
+    expect(subject[:language_product_id]).to eq(LanguageProduct.first.id)
+    expect(multi_size_attrs[:barcode]).to eq(Sku.find_by(sku: subject[:sku]).barcodes.first.barcode)
   end
 end
