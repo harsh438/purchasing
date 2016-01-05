2b8b6f01afa58a9735d77c9debd281293957d9cb
Chris Blackburn <christopher.blackburn@gmail.com>
2016-01-05 10:25:27 +0000

    Save code coverage output to Circle artifact directory

--- api/spec/support/code_coverage.rb
+++ api/spec/support/code_coverage.rb
@@ -1,5 +1,11 @@
 if ENV['COVERAGE'] == 'on'
   require 'simplecov'
+
+  if ENV['CIRCLE_ARTIFACTS']
+    artifacts_dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+    SimpleCov.coverage_dir(artifacts_dir)
+  end
+
   SimpleCov.start 'rails'
 
   if ENV['COVERAGE_FORMAT'] == 'rcov'
