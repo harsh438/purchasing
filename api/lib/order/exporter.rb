62b45a486b7784e64c0b1b575efc2fd72cece6c4
Fareed Dudhia <fareeddudhia@googlemail.com>
2016-01-05 11:47:55 +0000

    Fix size vs man size attrs in exporter

--- api/lib/order/exporter.rb
+++ api/lib/order/exporter.rb
@@ -84,8 +84,8 @@ class Order::Exporter
     language_product_option = language_option(order_line_item.product_id,
                                               order_line_item.option_id)
 
-    { product_size: language_product_option.try(:option).try(:size),
-      manufacturer_size: language_product_option.name }
+    { product_size: language_product_option.name,
+      manufacturer_size: language_product_option.try(:option).try(:size) }
   end
 
   def assign_po_to_orders(purchase_order, orders)
