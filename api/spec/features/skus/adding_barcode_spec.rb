63a4039653f617bf0340c57f7407fc74311e594f
Fareed Dudhia <fareeddudhia@googlemail.com>
2016-01-06 17:20:32 +0000

    Add test to ensure adding a barcode to a sku updates the sku reference on the sku table

--- api/spec/features/skus/adding_barcode_spec.rb
+++ api/spec/features/skus/adding_barcode_spec.rb
@@ -16,6 +16,11 @@ feature 'Adding a barcode to an existing sku' do
     then_legacy_records_should_not_be_created_for_the_sku
   end
 
+  scenario 'Change internal_sku when adding barcode to sku' do
+    when_i_add_a_barcode_to_a_sku_with_temporary_reference
+    then_the_internal_sku_should_be_updated
+  end
+
   def when_i_add_a_barcode_to_sku
     add_barcode_to_sku(create(:sku))
   end
@@ -25,7 +30,7 @@ feature 'Adding a barcode to an existing sku' do
   end
 
   def when_i_add_a_barcode_to_a_sku_for_the_first_time
-    add_barcode_to_sku(create(:sku_without_barcode))
+    add_barcode_to_sku(sku_without_barcode)
   end
 
   def then_legacy_records_should_be_created_for_the_sku
@@ -40,9 +45,19 @@ feature 'Adding a barcode to an existing sku' do
     expect(subject[:product_id]).to eq(sku.product_id)
   end
 
+  def when_i_add_a_barcode_to_a_sku_with_temporary_reference
+    add_barcode_to_sku(sku_without_barcode)
+  end
+
+  def then_the_internal_sku_should_be_updated
+    old_sku = sku_without_barcode.sku
+    expect(subject[:sku]).to_not eq(old_sku)
+  end
+
   private
 
   let(:sku) { create(:sku) }
+  let(:sku_without_barcode) { create(:sku_without_barcode) }
 
   def add_barcode_to_sku(sku)
     attrs = { barcodes_attributes: [{ barcode: '00000' }] }
