8f078db2665502bf3ab18a3135c66b60b5f1a360
Fareed Dudhia <fareeddudhia@googlemail.com>
2016-01-06 14:38:15 +0000

    Add with/without barcode checking to sku generation spec

--- api/spec/features/skus/generation_spec.rb
+++ api/spec/features/skus/generation_spec.rb
@@ -1,44 +1,75 @@
 feature 'SKU generation' do
   subject { JSON.parse(page.body).with_indifferent_access }
 
-  let (:multi_size_attrs) { { manufacturer_sku: 'DA-ADFADET-WHT',
-                              manufacturer_color: 'Pale Blue',
-                              manufacturer_size: '12',
-                              season: 'ss15',
-                              color: 'Blue',
-                              size: '06-9 mths',
-                              cost_price: 12.06,
-                              price: 18.00,
-                              lead_gender: 'M',
-                              product_name: 'Clarks Originals Boots - Clarks Originals Baby Warm - Pale Blue',
-                              vendor_id: 919,
-                              category_id: 12,
-                              category_name: 'Whatever',
-                              inv_track: 'P',
-                              barcode: '12389123' } }
+  let (:base_sku_attrs) { { manufacturer_sku: 'DA-ADFADET-WHT',
+                            manufacturer_color: 'Pale Blue',
+                            manufacturer_size: '12',
+                            season: 'ss15',
+                            color: 'Blue',
+                            size: '06-9 mths',
+                            cost_price: 12.06,
+                            price: 18.00,
+                            lead_gender: 'M',
+                            product_name: 'Clarks Originals Boots - Clarks Originals Baby Warm - Pale Blue',
+                            vendor_id: 919,
+                            category_id: 12,
+                            category_name: 'Whatever',
+                            inv_track: 'P' } }
 
-  scenario 'Generating skus using the API' do
-    when_i_generate_skus
+  let (:sku_with_barcode_attrs) { base_sku_attrs.merge!({ barcode: '12389123' }) }
+  let (:sku_with_no_barcode_attrs) { base_sku_attrs.merge!({ sku: 'NEGATIVE-EXAMPLE' }) }
+
+  let (:sku) { Sku.find_by(sku: subject[:sku]) }
+
+  scenario 'Generating skus with a barcode' do
+    when_i_generate_skus_from_attrs_with_a_barcode
+    then_correct_skus_and_legacy_records_should_be_generated
+  end
+
+  scenario 'Generating skus without a barcode' do
+    when_i_generate_skus_from_attrs_with_no_barcode
     then_correct_skus_should_be_generated
   end
 
-  def when_i_generate_skus
-    page.driver.post skus_path(multi_size_attrs)
+  def when_i_generate_skus_from_attrs_with_a_barcode
+    page.driver.post skus_path(sku_with_barcode_attrs)
+  end
+
+  def when_i_generate_skus_from_attrs_with_no_barcode
+    page.driver.post skus_path(sku_with_no_barcode_attrs)
+  end
+
+  def then_correct_skus_and_legacy_records_should_be_generated
+    check_correct_skus
+
+    expect(subject[:sku]).to eq("#{Product.first.id}-#{Element.first.id}")
+    expect(sku_with_barcode_attrs[:barcode]).to eq(Sku.find_by(sku: subject[:sku]).barcodes.first.barcode)
+
+    expect(sku.product).to be_a(Product)
+    expect(sku.language_product).to be_a(LanguageProduct)
+    expect(sku.language_product_option).to be_a(LanguageProductOption)
+    expect(sku.element).to be_a(Element)
+    expect(sku.option).to be_a(Option)
+    expect(sku.language_category).to be_a(LanguageCategory)
   end
 
   def then_correct_skus_should_be_generated
-    multi_size_attrs.except(:cost_price,
-                            :price,
-                            :lead_gender,
-                            :category_id,
-                            :category_name,
-                            :inv_track,
-                            :barcode).each do |key, a|
+    check_correct_skus
+
+    expect(subject[:sku]).to eq(sku_with_no_barcode_attrs[:sku])
+  end
+
+  private
+
+  def check_correct_skus
+    sku_with_barcode_attrs.except(:cost_price,
+                                  :price,
+                                  :lead_gender,
+                                  :category_id,
+                                  :category_name,
+                                  :inv_track,
+                                  :barcode).each do |key, a|
       expect(subject[key]).to eq(a)
     end
-
-    expect(subject[:sku]).to eq("#{Product.first.id}-#{Element.first.id}")
-    expect(subject[:language_product_id]).to eq(LanguageProduct.first.id)
-    expect(multi_size_attrs[:barcode]).to eq(Sku.find_by(sku: subject[:sku]).barcodes.first.barcode)
   end
 end
