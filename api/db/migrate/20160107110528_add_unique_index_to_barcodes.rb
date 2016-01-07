f0e2b2853496c39422a1f6051bdc9c8bab2c00e2
Luke <lukemorton.dev@gmail.com>
2016-01-07 13:19:22 +0000

    Nullify migration so we can continue prod deploys

--- api/db/migrate/20160107110528_add_unique_index_to_barcodes.rb
+++ api/db/migrate/20160107110528_add_unique_index_to_barcodes.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToBarcodes < ActiveRecord::Migration
   def change
-    add_index :barcodes, :barcode, unique: true
+    # add_index :barcodes, :barcode, unique: true
   end
 end
