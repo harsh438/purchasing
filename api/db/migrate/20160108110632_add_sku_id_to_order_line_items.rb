7e883d1e2212d2dd33be88d1157035a245efc538
Luke <lukemorton.dev@gmail.com>
2016-01-11 10:44:00 +0000

    Validation hack

--- api/db/migrate/20160108110632_add_sku_id_to_order_line_items.rb
+++ api/db/migrate/20160108110632_add_sku_id_to_order_line_items.rb
@@ -14,6 +14,7 @@ class AddSkuIdToOrderLineItems < ActiveRecord::Migration
   private
 
   def set_sku_id_on_order_line_item(order_line_item)
+    order_line_item.quantity = 1 if order_line_item.quantity < 1
     order_line_item.update!(sku: Sku.find_by!(sku: order_line_item.internal_sku))
   rescue ActiveRecord::RecordNotFound
     Rails.logger.error("sku=#{order_line_item.internal_sku}  order_line_item_id=#{order_line_item.id}")
