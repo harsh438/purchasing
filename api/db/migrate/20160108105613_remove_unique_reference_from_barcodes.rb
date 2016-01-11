61f221f22a7944cedb061e398ff1a350f0906c09
Luke <lukemorton.dev@gmail.com>
2016-01-11 09:42:21 +0000

    Update migration to only apply if index exists

--- api/db/migrate/20160108105613_remove_unique_reference_from_barcodes.rb
+++ api/db/migrate/20160108105613_remove_unique_reference_from_barcodes.rb
@@ -1,8 +1,5 @@
 class RemoveUniqueReferenceFromBarcodes < ActiveRecord::Migration
   def change
-    # unless prod condition added since we never successfully
-    # applied this migration to prod and no longer want to
-    #
-    remove_index :barcodes, :barcode unless Rails.env.production?
+    remove_index(:barcodes, :barcode) if index_exists?(:barcodes, :barcode)
   end
 end
