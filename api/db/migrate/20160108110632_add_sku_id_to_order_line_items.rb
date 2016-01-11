5c87c87b719bbbee0517347494a95aea6585071f
Luke <lukemorton.dev@gmail.com>
2016-01-11 10:04:37 +0000

    Extract out single migration so we can raise more detailed error

--- api/db/migrate/20160108110632_add_sku_id_to_order_line_items.rb
+++ api/db/migrate/20160108110632_add_sku_id_to_order_line_items.rb
@@ -4,10 +4,16 @@ class AddSkuIdToOrderLineItems < ActiveRecord::Migration
 
     reversible do |dir|
       dir.up do
-        OrderLineItem.all.each do |order_line_item|
-          order_line_item.update!(sku: Sku.find_by!(sku: order_line_item.internal_sku))
-        end
+        OrderLineItem.all.each(&method(:set_sku_id_on_order_line_item))
       end
     end
+  end
+
+  private
+
+  def set_sku_id_on_order_line_item(order_line_item)
+    order_line_item.update!(sku: Sku.find_by!(sku: order_line_item.internal_sku))
+  rescue ActiveRecord::RecordNotFound => e
+    raise "sku=#{order_line_item.internal_sku}  order_line_item_id=#{order_line_item.id}\n"
   end
 end
