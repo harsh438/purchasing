abe33dcd88f239a4b24f46489f17ac35182b6da9
Luke <lukemorton.dev@gmail.com>
2016-01-11 12:54:56 +0000

    Fix migration

--- api/db/migrate/20160108114603_add_sku_id_to_purchase_orders.rb
+++ api/db/migrate/20160108114603_add_sku_id_to_purchase_orders.rb
@@ -1,5 +1,11 @@
 class AddSkuIdToPurchaseOrders < ActiveRecord::Migration
   def change
-    add_reference :purchase_orders, :sku, index: true
+    if column_exists?(:purchase_orders, :sku_id)
+      unless index_exists?(:purchase_orders, :sku_id)
+        add_index :purchase_orders, :sku_id
+      end
+    else
+      add_reference :purchase_orders, :sku, index: true
+    end
   end
 end
