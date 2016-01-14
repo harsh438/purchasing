6d1fc0e4dcf137103ae2f8f9718f563119cc1615
Luke <lukemorton.dev@gmail.com>
2016-01-14 08:56:56 +0000

    Remove comments and logger

--- api/db/migrate/20160114084044_add_sku_id_to_latest_po.rb
+++ api/db/migrate/20160114084044_add_sku_id_to_latest_po.rb
@@ -1,15 +1,5 @@
 class AddSkuIdToLatestPo < ActiveRecord::Migration
   def change
-    # PurchaseOrderLineItem.where(po_number: '96122').each do |po_line_item|
-    #   sku = Sku.find_by(product_name: po_line_item.brand_product_name,
-    #                     brand_size: po_line_item.brand_size)
-    #
-    #   if sku.present?
-    #     po_line_item.update!(sku: sku)
-    #   end
-    # end
-
-    PurchaseOrderLineItem.logger = Logger.new(STDOUT)
     PurchaseOrderLineItem.joins('JOIN skus ON skus.product_name = purchase_orders.orderTool_productName
                                  AND skus.manufacturer_size = purchase_orders.orderTool_brandSize')
                          .where(po_number: '96122')
