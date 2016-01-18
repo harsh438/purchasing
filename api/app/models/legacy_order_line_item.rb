9914c0d3153f1b3e5a01cc1620977124c5b772b2
Luke <lukemorton.dev@gmail.com>
2016-01-18 17:44:19 +0000

    Revert "Add erroneous order fixer"
    This reverts commit 64f4dbaf5d932bce4ff9197ee6c18774b5ee54a1.

--- api/app/models/legacy_order_line_item.rb
+++
@@ -1,7 +0,0 @@
-class LegacyOrderLineItem < ActiveRecord::Base
-  self.table_name = 'order_data_details_temp'
-
-  has_many :sizes, class_name: 'LegacyOrderLineItemSize',
-                   foreign_key: :OrderID,
-                   primary_key: :OrderID
-end
