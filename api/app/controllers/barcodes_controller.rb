91f95ab04ad47f7c34db195c62a0d46ea8dd791c
Fareed Dudhia <fareeddudhia@googlemail.com>
2016-01-22 16:56:33 +0000

    Add barcode exception handling for bad records

--- api/app/controllers/barcodes_controller.rb
+++ api/app/controllers/barcodes_controller.rb
@@ -3,13 +3,24 @@ class BarcodesController < ApplicationController
     render json: imported_barcodes
   rescue ActiveRecord::RecordNotFound
     render json: { errors: { skus: ['Some SKUs were not recognised so we did not import any barcodes'] },
-                   nonexistant_skus: nonexistant_skus }
+           nonexistant_skus: nonexistant_skus }
+  rescue ActiveRecord::RecordInvalid
+    render json: { errors: { skus: ['Some records were missing or invalid'] },
+           nonexistant_skus: bad_barcodes }
   end
 
   private
 
   def barcodes
     params.require(:barcodes)
+  end
+
+  def bad_barcodes
+    barcodes.map do |barcode|
+      barcode unless barcode[:sku].present? and
+                     barcode[:brand_size].present? and
+                     barcode[:barcode].present?
+    end.compact
   end
 
   def imported_barcodes
