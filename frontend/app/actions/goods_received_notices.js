45762e39db073c3ddaf69a2eb4033494c0d8402e
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-01 13:01:01 +0000

    Showing only 2 weeks before and 2 weeks after

--- frontend/app/actions/goods_received_notices.js
+++ frontend/app/actions/goods_received_notices.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 
 export function loadGoodsReceivedNotices(middleWeekStart) {
   const startDate = moment(middleWeekStart, 'DD/MM/YYYY').subtract(2, 'weeks').format('YYYY-MM-DD');
-  const endDate = moment(middleWeekStart, 'DD/MM/YYYY').add({ weeks: 2, days: 5 }).format('YYYY-MM-DD');
+  const endDate = moment(middleWeekStart, 'DD/MM/YYYY').add({ weeks: 2 }).format('YYYY-MM-DD');
 
   return dispatch => {
     const queryString = Qs.stringify({ start_date: startDate, end_date: endDate });
