438242bed5ddf42387444bd533fa74bca7f524ec
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-02 12:29:09 +0000

    Revert "Renaming CSVExporter to Exporter to reflect the new logic"
    This reverts commit d12f27a1a4b1986bde23585adee68faea3360f19.

---
+++ api/lib/sku/csv_exporter.rb
@@ -0,0 +1,36 @@
+class Sku::CsvExporter
+  def export(params)
+    csv = Table::ViewModel.new
+    csv << columns
+
+    if params[:order_id].present?
+      order = Order.find(params[:order_id])
+      csv.concat(skus_in_order(order))
+    elsif params[:purchase_order_id].present?
+      purchase_order = PurchaseOrder.find(params[:purchase_order_id])
+      csv.concat(skus_in_purchase_order(purchase_order))
+    end
+  end
+
+  private
+
+  def columns
+    [:internal_sku, :barcode]
+  end
+
+  def skus_in_order(order)
+    order.purchase_orders.flat_map { |po| skus_in_purchase_order(po) }
+  end
+
+  def skus_in_purchase_order(po)
+    po.line_items.map do |po_line_item|
+      values_from_sku(Sku.find_by(sku: po_line_item.internal_sku))
+    end.compact
+  end
+
+  def values_from_sku(sku)
+    if sku.present?
+      [sku.sku, sku.barcodes.first.try(:barcode)]
+    end
+  end
+end
