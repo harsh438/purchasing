9eac146618aa04341586779aebabb6b1deb30396
Luke <lukemorton.dev@gmail.com>
2016-02-02 12:10:29 +0000

    Enforce table name prefix to be correct db

--- api/app/models/concerns/booking_in_connection.rb
+++ api/app/models/concerns/booking_in_connection.rb
@@ -4,4 +4,11 @@ module BookingInConnection
   included do
     establish_connection "bookingin_#{Rails.env}".to_sym
   end
+
+  module ClassMethods
+    def table_name_prefix
+      database = Rails.configuration.database_configuration[Rails.env.to_s]['database']
+      "#{database}."
+    end
+  end
 end
