66626ee5c16d16c130568d517659e701ab418339
Luke <lukemorton.dev@gmail.com>
2016-02-02 13:55:25 +0000

    Ensure database name prefixed to table name

--- api/app/models/concerns/booking_in_connection.rb
+++ api/app/models/concerns/booking_in_connection.rb
@@ -2,13 +2,7 @@ module BookingInConnection
   extend ActiveSupport::Concern
 
   included do
+    self.table_name = "#{Rails.configuration.database_configuration[Rails.env.to_s]['database']}.#{self.table_name}"
     establish_connection "bookingin_#{Rails.env}".to_sym
-  end
-
-  module ClassMethods
-    def table_name_prefix
-      database = Rails.configuration.database_configuration[Rails.env.to_s]['database']
-      "#{database}."
-    end
   end
 end
