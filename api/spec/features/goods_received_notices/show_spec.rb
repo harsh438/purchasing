d9fc3050a5d95ff53254a39f47ab75a875ded7d3
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-03 11:51:04 +0000

    Adding a test for the show grn api

---
+++ api/spec/features/goods_received_notices/show_spec.rb
@@ -0,0 +1,16 @@
+feature 'Show GRN' do
+  scenario 'Fetching a single GRN' do
+    when_i_request_a_grn_which_exists
+    then_i_should_have_a_grn_as_json
+  end
+
+  def when_i_request_a_grn_which_exists
+    grn = create(:goods_received_notice)
+    visit goods_received_notice_path(grn.id)
+  end
+
+  def then_i_should_have_a_grn_as_json
+    grn = JSON.parse page.body
+    expect(grn).to include("units_received", "cartons_received", "delivery_date", "id")
+  end
+end
