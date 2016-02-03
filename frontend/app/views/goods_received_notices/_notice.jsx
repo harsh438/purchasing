df93f03858620a468aaa958ad381fb0f8a2ac751
Luke <lukemorton.dev@gmail.com>
2016-02-03 12:08:36 +0000

    Ensure currently editing notice is blue not status color

--- frontend/app/views/goods_received_notices/_notice.jsx
+++ frontend/app/views/goods_received_notices/_notice.jsx
@@ -22,9 +22,18 @@ export default class GoodsReceivedNoticesNotice extends React.Component {
   }
 
   containerClass() {
-    let classes = `grn_notice list-group-item ${this.statusClass()}`;
-    if (this.props.compact) classes += ' grn_notice--compact';
-    if (this.props.id === this.props.currentGoodsReceivedNotice.id) classes += ' list-group-item-info';
+    let classes = `grn_notice list-group-item`;
+
+    if (this.props.compact) {
+      classes += ' grn_notice--compact';
+    }
+
+    if (this.props.id === this.props.currentGoodsReceivedNotice.id) {
+      classes += ' list-group-item-info';
+    } else {
+      classes += ` ${this.statusClass()}`;
+    }
+
     return classes;
   }
 
@@ -32,7 +41,7 @@ export default class GoodsReceivedNoticesNotice extends React.Component {
     return "grn_notice__badge badge";
   }
 
-  statusClass(prefix) {
+  statusClass() {
     switch (this.props.status) {
     case 'late':
       return 'grn_notice--late list-group-item-danger';
