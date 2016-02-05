abf5d84f82e059da5b40d0e9ea62ef2fb3bc5256
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-05 16:57:37 +0000

    Adding file upload validation

--- api/app/models/packing_list.rb
+++ api/app/models/packing_list.rb
@@ -1,2 +1,20 @@
 class PackingList < ActiveRecord::Base
+  belongs_to :goods_received_notice
+  has_attached_file :list
+
+  validates_attachment_content_type :list, content_type: %w(image/jpeg
+                                                            image/pjpeg
+                                                            image/png
+                                                            image/x-png
+                                                            application/pdf
+                                                            application/vnd.ms-excel,
+                                                            application/msexcel,
+                                                            application/x-msexcel,
+                                                            application/x-ms-excel,
+                                                            application/x-excel,
+                                                            application/x-dos_ms_excel,
+                                                            application/xls,
+                                                            application/x-xls,
+                                                            application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
+                                                            application/x-pdf)
 end
