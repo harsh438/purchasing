a93cc47b94c7ccd9fed59969dc27bcea60092bd3
Luke <lukemorton.dev@gmail.com>
2016-02-10 11:33:28 +0000

    Use transactions around examples

--- api/spec/support/database_cleaner.rb
+++ api/spec/support/database_cleaner.rb
@@ -1,10 +1,16 @@
 require 'database_cleaner'
-DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
-  config.before :example do
-    DatabaseCleaner.clean
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 end
