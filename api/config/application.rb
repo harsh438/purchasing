551105b0c8b4ca9ab6b3b8ee1affc5ea3652558e
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-11 15:32:35 +0000

    Controllers in the hub folder are now included

--- api/config/application.rb
+++ api/config/application.rb
@@ -10,6 +10,7 @@ module Purchasing
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('app/controllers/hub')
 
     config.http_auth = { enabled: true,
                          username: 'purchasing',
