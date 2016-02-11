afddfb36e3200a589069a1201b4992566cfdb140
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-11 10:56:04 +0000

    Code styling

--- api/app/models/packing_list.rb
+++ api/app/models/packing_list.rb
@@ -1,29 +1,30 @@
 class PackingList < ActiveRecord::Base
+  CONTENT_TYPES = %w(image/jpeg
+                     image/pjpeg
+                     image/png
+                     image/x-png
+                     application/pdf
+                     application/vnd.ms-excel
+                     application/msexcel
+                     application/x-msexcel
+                     application/x-ms-excel
+                     application/x-excel
+                     application/x-dos_ms_excel
+                     application/xls
+                     application/x-xls
+                     application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+                     application/x-pdf
+                     message/rfc822
+                     text/html
+                     message/rfc822 eml
+                     application/msword
+                     application/vnd.openxmlformats-officedocument.wordprocessingml.document
+                     application/vnd.openxmlformats-officedocument.wordprocessingml.template)
+
   belongs_to :goods_received_notice
   has_attached_file :list
 
-  validates_attachment_content_type :list, content_type: %w(image/jpeg
-                                                            image/pjpeg
-                                                            image/png
-                                                            image/x-png
-                                                            application/pdf
-                                                            application/vnd.ms-excel,
-                                                            application/msexcel,
-                                                            application/x-msexcel,
-                                                            application/x-ms-excel,
-                                                            application/x-excel,
-                                                            application/x-dos_ms_excel,
-                                                            application/xls,
-                                                            application/x-xls,
-                                                            application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
-                                                            application/x-pdf
-                                                            message/rfc822
-                                                            text/html
-                                                            message/rfc822 eml
-                                                            application/msword
-                                                            application/vnd.openxmlformats-officedocument.wordprocessingml.document
-                                                            application/vnd.openxmlformats-officedocument.wordprocessingml.template
-                                                            )
+  validates_attachment_content_type :list, content_type: CONTENT_TYPES
   def list_url
     list.expiring_url(300)
   end
