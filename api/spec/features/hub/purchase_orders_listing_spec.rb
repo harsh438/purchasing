f8b6e04ae861eaea25d1a30951a2ea7fe9a60eb5
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-12 14:23:00 +0000

    Updating the test to check that we receive the right request id

--- api/spec/features/hub/purchase_orders_listing_spec.rb
+++ api/spec/features/hub/purchase_orders_listing_spec.rb
@@ -9,14 +9,19 @@ feature 'Listing Purchase Orders for the hub' do
   def when_i_request_a_list_of_purchase_orders
     create_purchase_order_with_line_items
     page.driver.post latest_hub_purchase_orders_path, {
-      parameters: { timestamp_from: Date.yesterday.iso8601 }
+      parameters: { timestamp_from: Date.yesterday.iso8601, request_id: request_id }
     }
   end
 
   def then_i_should_get_purchase_orders_with_line_items
-    expect(subject.count).to be(10)
-    expect(subject[0]['purchase_order_line_items'][0]).to match(a_hash_including('status', 'delivery_date'))
+    expect(subject['request_id']).to eq(request_id)
+    expect(subject['orders'].count).to be(10)
+    expect(subject['orders'][0]['purchase_order_line_items'][0]).to match(a_hash_including('status', 'delivery_date'))
   end
+
+  let (:request_id) {
+    'gxrjcvvu2h4kppijw04nf3hxvz8dc97w'
+  }
 
   let (:create_purchase_order_with_line_items) do
     create_list(:purchase_order, 10, :with_line_items)
