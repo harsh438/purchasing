bb7cfb9dbc6c9e0f6524ca2be2af381f907a0378
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-12 15:04:51 +0000

    Request id is now outside of the paramters

--- api/app/controllers/hub/purchase_orders_controller.rb
+++ api/app/controllers/hub/purchase_orders_controller.rb
@@ -1,7 +1,7 @@
 class Hub::PurchaseOrdersController < ApplicationController
   def latest
     timestamp_from = params[:parameters][:timestamp_from]
-    request_id = params[:parameters][:request_id]
+    request_id = params[:request_id]
     render json: {
       request_id: request_id,
       orders: PurchaseOrder.where('drop_date > ?', Time.parse(timestamp_from))
