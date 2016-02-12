afe7ef393d69e171363c1aa5f5541230608c1301
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-12 12:10:30 +0000

    Trying to limit the amount of data

--- api/app/controllers/hub/purchase_orders_controller.rb
+++ api/app/controllers/hub/purchase_orders_controller.rb
@@ -3,6 +3,7 @@ class Hub::PurchaseOrdersController < ApplicationController
     timestamp_from = params[:parameters][:timestamp_from]
     render json: PurchaseOrder.where('drop_date > ?', Time.parse(timestamp_from))
                               .includes_line_items
+                              .limit(10)
                               .map(&:as_json_with_line_items)
   end
 end
