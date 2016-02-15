f3605567d9a3de90178e9ece3f88f0525424ef0c
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-15 12:19:43 +0000

    The limit is now dynamic

--- api/app/controllers/hub/purchase_orders_controller.rb
+++ api/app/controllers/hub/purchase_orders_controller.rb
@@ -1,10 +1,11 @@
 class Hub::PurchaseOrdersController < ApplicationController
   def latest
     timestamp_from = params[:parameters][:timestamp_from]
+    limit = params[:parameters] || 10
     request_id = params[:request_id]
     results = PurchaseOrder.where('drop_date > ?', Time.parse(timestamp_from))
                            .includes_line_items
-                           .limit(10)
+                           .limit(limit)
     render json: {
       request_id: request_id,
       purchase_orders: ActiveModel::ArraySerializer.new(
