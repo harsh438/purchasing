1f758db84bbbda6076f42d5c8d60bc3bb369224b
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-15 15:45:46 +0000

    Adding some necessary traits for the hub puchasing test

--- api/spec/factories/purchase_order_factory.rb
+++ api/spec/factories/purchase_order_factory.rb
@@ -7,12 +7,20 @@ FactoryGirl.define do
       line_items { build_list(:purchase_order_line_item, 4, vendor: vendor) }
     end
 
+    trait :with_line_items_with_barcode do
+      line_items { build_list(:purchase_order_line_item, 4, :with_barcode, vendor: vendor) }
+    end
+
     trait :with_balance_line_items do
       line_items { build_list(:purchase_order_line_item, 2, :balanced, vendor: vendor) }
     end
 
     trait :with_old_drop_date do
       drop_date { 1.year.ago }
+    end
+
+    trait :with_grn_events do
+      goods_received_notice_events { build_list(:goods_received_notice_event, 4, :for_today) }
     end
   end
 end
