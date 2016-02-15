29521b46df8cc107856078b733666d0ac0d16d48
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-15 10:31:26 +0000

    exit on errors

--- lint.sh
+++ lint.sh
@@ -2,17 +2,19 @@
 if `docker-compose run web rake cane`; then
     echo "[+] CANE - Success";
 else
-    echo ""
+    exit 1;
 fi
 if `docker-compose run web rake tailor > /tmp/tailor.tmp.log`; then
     echo "[+] TAILOR - Success";
 else
     cat /tmp/tailor.tmp.log;
+    exit 1;
 fi
 if `docker-compose run frontend npm run-script build > /tmp/frontend-lint.tmp.log`; then
     echo "[+] Frontend build - Success";
 else
     cat /tmp/front-end-lint.tmp.log;
+    exit 1;
 fi
 
 
