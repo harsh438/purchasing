c2a4cacc53a50110db3b5ca8443c7b41bb396c25
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-15 12:42:47 +0000

    only sending the ones which were not in sync in pvx

--- api/app/controllers/hub/purchase_orders_controller.rb
+++ api/app/controllers/hub/purchase_orders_controller.rb
@@ -3,8 +3,9 @@ class Hub::PurchaseOrdersController < ApplicationController
     timestamp_from = params[:parameters][:timestamp_from]
     limit = params[:parameters][:limit] || 10
     request_id = params[:request_id]
-    results = PurchaseOrder.where('drop_date > ?', Time.parse(timestamp_from))
+    results = PurchaseOrder.where('po_summary.drop_date > ?', Time.parse(timestamp_from))
                            .includes_line_items
+                           .not_sent_in_peoplevox
                            .limit(limit)
     render json: {
       request_id: request_id,
