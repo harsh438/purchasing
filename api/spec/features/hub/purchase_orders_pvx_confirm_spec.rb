368cc881e123005245d42b9cc27a1c6810f0bb94
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-15 17:08:31 +0000

    Adding a new test to assert that when we mark a purchase order as processed, we should not have it again

---
+++ api/spec/features/hub/purchase_orders_pvx_confirm_spec.rb
@@ -0,0 +1,32 @@
+feature 'PVX confirm API for the hub' do
+  subject { JSON.parse(page.body) }
+
+  scenario 'Confirmation of PVX' do
+    when_i_confirm_that_a_purchase_order_was_sent_in_peoplevox
+    then_it_should_be_removed_from_the_list
+  end
+
+  def when_i_confirm_that_a_purchase_order_was_sent_in_peoplevox
+    page.driver.post pvx_confirm_hub_purchase_orders_path, {
+      purchase_order: { id: purchase_order_list[0].id }, request_id: request_id
+    }
+  end
+
+  def then_it_should_be_removed_from_the_list
+    page.driver.post latest_hub_purchase_orders_path, {
+      parameters: { timestamp_from: Date.yesterday.iso8601 }, request_id: request_id
+    }
+    expect(subject['purchase_orders'].count).to eq(4)
+    expect(subject['purchase_orders'].map do |line_item|
+      line_item['id']
+    end.not_to eq(purchase_order_list[0].id)
+  end
+
+  let (:request_id) {
+    'gxrjcvvu2h4kppijw04nf3hxvz8dc97w'
+  }
+
+  let (:purchase_order_list) do
+    create_list(:purchase_order, 5, :with_line_items_with_barcode, :with_grn_events)
+  end
+end
