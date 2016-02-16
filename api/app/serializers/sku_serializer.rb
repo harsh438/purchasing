7a8f47221fc9272f272319713e8710f06db0789f
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-16 17:33:25 +0000

    Adding new defaults for the sku hub schema

--- api/app/serializers/sku_serializer.rb
+++ api/app/serializers/sku_serializer.rb
@@ -12,15 +12,43 @@ class SkuSerializer < ActiveModel::Serializer
   attributes :color
   attributes :size
 
+  def id
+    object.sku
+  end
+
+  def category_name
+    object.language_category.try(:name)
+  end
+
   def name
     object.product_name
   end
 
   def barcode
-    object.product.barcode
+    object.barcodes.try(:[], 0).try(:barcode) || ''
   end
 
   def retail_price
-    object.list_price
+    object.list_price || 0
+  end
+
+  def cost_price
+    object.cost_price || 0
+  end
+
+  def manufacturer_color
+    object.manufacturer_color || ''
+  end
+
+  def manufacturer_size
+    object.manufacturer_size || ''
+  end
+
+  def color
+    object.color || ''
+  end
+
+  def size
+    object.size || ''
   end
 end
