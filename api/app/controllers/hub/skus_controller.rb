fbce2813b58e9cc6e677086845b40b473d9bf360
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-16 15:02:47 +0000

    Adding new pvx confirm endpoint

--- api/app/controllers/hub/skus_controller.rb
+++ api/app/controllers/hub/skus_controller.rb
@@ -1,16 +1,26 @@
 class Hub::SkusController < ApplicationController
   def latest
     request_id = params[:request_id]
-    limit = 200
+    limit = 50
     if params[:parameters] and params[:parameters][:limit]
       limit = params[:parameters][:limit]
     end
+    skus = Sku.not_sent_in_peoplevox.with_barcode.limit(limit)
     render json: {
       request_id: request_id,
       skus: ActiveModel::ArraySerializer.new(
-        Sku.with_barcode.limit(10),
+        skus,
         each_serializer: SkuSerializer
       )
     }
+  end
+
+  def pvx_confirm
+    sku_id = params[:sku][:id]
+    sku = Sku.find(sku_id)
+    sku.sent_in_peoplevox = DateTime.now
+    sku.save!
+    message = "Sku #{sku_id} has been marked as sent to PeopleVox and won't be processed again."
+    render json: { message: message }
   end
 end
