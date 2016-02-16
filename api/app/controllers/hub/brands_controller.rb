43323676d8ca4cead0fb56e59acf7471b6510722
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-16 09:48:33 +0000

    Adding PVX Confirmation endpoint for brands

--- api/app/controllers/hub/brands_controller.rb
+++ api/app/controllers/hub/brands_controller.rb
@@ -5,7 +5,7 @@ class Hub::BrandsController < ApplicationController
     if params[:parameters] and params[:parameters][:limit]
       limit = params[:parameters][:limit]
     end
-    results = Vendor.limit(limit)
+    results = Vendor.not_sent_in_peoplevox.limit(limit)
     render json: {
       request_id: request_id,
       brands: ActiveModel::ArraySerializer.new(
@@ -13,5 +13,13 @@ class Hub::BrandsController < ApplicationController
         each_serializer: ::BrandSerializer
       )
     }
+  end
+
+  def pvx_confirm
+    brand_id = params[:brand][:id]
+    brand = Vendor.find(brand_id)
+    brand.sent_in_peoplevox = DateTime.now
+    brand.save!
+    render json: { message: "Order #{brand_id} has now been marked as being sent in peoplevox" }
   end
 end
