8b3c6f49bdc8fea08a53c55ac66a64a4234b71a1
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-16 17:33:02 +0000

    Adding existing language_category association

--- api/app/models/sku.rb
+++ api/app/models/sku.rb
@@ -17,6 +17,7 @@ class Sku < ActiveRecord::Base
   belongs_to :language_category, foreign_key: :category_id
   belongs_to :option, foreign_key: :option_id
   belongs_to :language_product_option, foreign_key: :language_product_option_id
+  belongs_to :language_category, foreign_key: :language_product_id
 
   has_many :barcodes
   accepts_nested_attributes_for :barcodes
