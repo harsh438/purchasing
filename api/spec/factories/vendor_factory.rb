cfe72c403732939e4a5b64ecdac8b10fc909420b
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-17 15:27:17 +0000

    Adding trait to customise the updated_at date

--- api/spec/factories/vendor_factory.rb
+++ api/spec/factories/vendor_factory.rb
@@ -5,5 +5,18 @@ FactoryGirl.define do
     trait :with_details do
       discontinued false
     end
+
+    trait :without_updated_date do
+      updated_at nil
+      record_timestamps false
+    end
+
+    trait :with_old_updated_date do
+      updated_at 1.year.ago
+    end
+
+    trait :with_recent_updated_date do
+      updated_at 5.minutes.ago
+    end
   end
 end
