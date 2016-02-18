828d71d11dbcd209b4b8e462ba4b94769b938601
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-18 09:56:09 +0000

    Refactoring

--- api/app/controllers/hub/skus_controller.rb
+++ api/app/controllers/hub/skus_controller.rb
@@ -7,23 +7,12 @@ class Hub::SkusController < ApplicationController
     last_timestamp = default_param(request_params[:last_timestamp], Time.now)
     last_id = default_param(request_params[:last_id], 0)
 
-    results = Sku.includes(:language_category)
-              .where('(skus.updated_at >= ? or skus.updated_at is null) and skus.id > ?', last_timestamp, last_id)
-              .with_barcode
-              .order(updated_at: :asc, id: :asc)
-              .limit(limit)
-    render json: {
-      request_id: request_id,
-      summary: "Returned #{results.size} sku objects.",
-      skus: ActiveModel::ArraySerializer.new(
-        results,
-        each_serializer: SkuSerializer
-      ),
-      parameters: {
-        last_timestamp: results.last.try(:updated_at) || last_timestamp,
-        last_id: results.last.try(:id) || last_id
-      }
-    }
+    skus = Sku.includes(:language_category)
+                 .where('(skus.updated_at >= ? or skus.updated_at is null) and skus.id > ?', last_timestamp, last_id)
+                 .with_barcode
+                 .order(updated_at: :asc, id: :asc)
+                 .limit(limit)
+    render json: create_hub_object(skus, request_id, last_timestamp, last_id)
   end
 
   def pvx_confirm
@@ -36,6 +25,21 @@ class Hub::SkusController < ApplicationController
   end
 
   private
+
+  def create_hub_object(skus, request_id, last_timestamp, last_id)
+    {
+      request_id: request_id,
+      summary: "Returned #{skus.size} sku objects.",
+      skus: ActiveModel::ArraySerializer.new(
+        skus,
+        each_serializer: SkuSerializer
+      ),
+      parameters: {
+        last_timestamp: skus.last.try(:updated_at) || last_timestamp,
+        last_id: skus.last.try(:id) || last_id
+      }
+    }
+  end
 
   def default_param(param, default_val)
     param.present? ? param : default_val
