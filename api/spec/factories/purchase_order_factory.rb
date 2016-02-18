8c90dfd1bd95300d5d64d934b46bbafa3131a596
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-18 14:03:31 +0000

    Adding traits for the updated timestamp

--- api/spec/factories/purchase_order_factory.rb
+++ api/spec/factories/purchase_order_factory.rb
@@ -23,6 +23,19 @@ FactoryGirl.define do
       drop_date { 1.year.ago }
     end
 
+    trait :without_updated_date do
+      updated_at nil
+      record_timestamps false
+    end
+
+    trait :with_old_updated_date do
+      updated_at 1.year.ago
+    end
+
+    trait :with_recent_updated_date do
+      updated_at 5.minutes.ago
+    end
+
     trait :with_grn_events do
       goods_received_notice_events { build_list(:goods_received_notice_event, 4, :for_today) }
     end
