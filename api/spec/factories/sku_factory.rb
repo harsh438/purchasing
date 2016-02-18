2a99816d0e934cc586dd0db0174e731568c52479
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-18 09:51:02 +0000

    Adding some traits to modify the updated_at date

--- api/spec/factories/sku_factory.rb
+++ api/spec/factories/sku_factory.rb
@@ -28,6 +28,19 @@ FactoryGirl.define do
 
     vendor
 
+    trait :without_updated_date do
+      updated_at nil
+      record_timestamps false
+    end
+
+    trait :with_old_updated_date do
+      updated_at 1.year.ago
+    end
+
+    trait :with_recent_updated_date do
+      updated_at 5.minutes.ago
+    end
+
     factory :sku do
       product_id { create(:product, manufacturer_sku: 'MANU-FACTURER-SKU-11-reddish').id }
       option_id { create(:option).id }
