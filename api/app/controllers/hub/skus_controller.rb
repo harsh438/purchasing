95f397106ce733c169b2ba145e1cfebe4a0f684e
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-18 09:50:41 +0000

    Updating the sku to the new hub logic

--- api/app/controllers/hub/skus_controller.rb
+++ api/app/controllers/hub/skus_controller.rb
@@ -1,17 +1,28 @@
 class Hub::SkusController < ApplicationController
   def latest
     request_id = params[:request_id]
-    limit = 50
-    if params[:parameters] and params[:parameters][:limit]
-      limit = params[:parameters][:limit]
-    end
-    skus = Sku.includes(:language_category).not_sent_in_peoplevox.with_barcode.limit(limit)
+    request_params = params[:parameters]
+
+    limit = default_param(request_params[:limit], 200)
+    last_timestamp = default_param(request_params[:last_timestamp], Time.now)
+    last_id = default_param(request_params[:last_id], 0)
+
+    results = Sku.includes(:language_category)
+              .where('(skus.updated_at >= ? or skus.updated_at is null) and skus.id > ?', last_timestamp, last_id)
+              .with_barcode
+              .order(updated_at: :asc, id: :asc)
+              .limit(limit)
     render json: {
       request_id: request_id,
+      summary: "Returned #{results.size} sku objects.",
       skus: ActiveModel::ArraySerializer.new(
-        skus,
+        results,
         each_serializer: SkuSerializer
-      )
+      ),
+      parameters: {
+        last_timestamp: results.last.try(:updated_at) || last_timestamp,
+        last_id: results.last.try(:id) || last_id
+      }
     }
   end
 
@@ -22,5 +33,11 @@ class Hub::SkusController < ApplicationController
     sku.save!
     message = "Sku #{sku_id} has been marked as sent to PeopleVox and won't be processed again."
     render json: { message: message }
+  end
+
+  private
+
+  def default_param(param, default_val)
+    param.present? ? param : default_val
   end
 end
