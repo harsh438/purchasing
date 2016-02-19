1c227fbbf8a1c4533f0faa2041445d27b87f8c37
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-19 09:26:16 +0000

    Updating the brand hub test to reflect the new sync logic

--- api/spec/features/hub/brands_listing_spec.rb
+++ api/spec/features/hub/brands_listing_spec.rb
@@ -1,5 +1,7 @@
 feature 'Listing Brands for the hub' do
-  subject { JSON.parse(page.body) }
+  def subject
+    JSON.parse(page.body)
+  end
 
   scenario 'Requesting a list of brands' do
     when_i_request_a_list_of_brands
@@ -31,13 +33,17 @@ feature 'Listing Brands for the hub' do
     then_i_should_have_valid_data_returned
   end
 
+  scenario 'Updating an existing brand' do
+    when_i_update_an_existing_brand_and_request_it
+    then_i_should_have_the_updated_brand_only
+  end
+
   def when_i_request_a_list_of_brands
     create_brands
     page.driver.post latest_hub_brands_path, {
       request_id: request_id,
       parameters: {
         limit: 40,
-        last_timestamp: Time.now.iso8601,
         last_id: 0
       }
     }
@@ -53,18 +59,43 @@ feature 'Listing Brands for the hub' do
     create_brands
     page.driver.post latest_hub_brands_path, {
       request_id: request_id,
-      parameters: {
-        limit: 5,
-        last_timestamp: Time.now.iso8601,
-        last_id: 0
-      }
+      parameters: { limit: 4, last_id: brands_without_updated_date.first.id - 1 }
     }
   end
 
   def then_i_should_get_a_list_of_brands_with_that_limit
     request_id_should_be_identical
-    expect(subject['brands'].count).to be(5)
+    expect(subject['brands'].count).to be(4)
+    last_id_should_be(brands_without_updated_date[3].id.to_s)
+    timestamp_returned_should_be_empty
     brand_should_contain_brand_fields
+
+    then_i_request_the_next_page(items: 4)
+
+    request_id_should_be_identical
+    expect(subject['brands'].count).to be(4)
+    timestamp_returned_should_be_empty
+    brand_should_contain_brand_fields
+
+    then_i_request_the_next_page(items: 4)
+    timestamp_should_be_recent
+    expect(subject['brands'].count).to be(2)
+    brand_should_contain_brand_fields
+
+    then_i_request_the_next_page(items: 4)
+    no_objects_should_be_returned
+    timestamp_should_be_recent
+  end
+
+  def then_i_request_the_next_page(params)
+    page.driver.post latest_hub_brands_path, {
+      request_id: request_id,
+      parameters: {
+        limit: params[:items],
+        last_id: subject['parameters']['last_id'],
+        last_timestamp: subject['parameters']['last_timestamp'],
+      }
+    }
   end
 
   def when_i_request_a_list_of_brands_within_timestamp
@@ -81,7 +112,7 @@ feature 'Listing Brands for the hub' do
 
   def then_i_should_get_a_list_of_brands_within_timestamp
     request_id_should_be_identical
-    expect(subject['brands'].count).to be(15)
+    expect(subject['brands'].count).to be(1)
     brand_should_contain_brand_fields
   end
 
@@ -91,7 +122,6 @@ feature 'Listing Brands for the hub' do
       request_id: request_id,
       parameters: {
         limit: 40,
-        last_timestamp: Time.now.iso8601,
         last_id: Vendor.first.id
       }
     }
@@ -105,21 +135,57 @@ feature 'Listing Brands for the hub' do
 
   def when_i_request_a_list_of_brands_with_id_limit_and_timestamp
     create_brands
+    fixed_date = brands_with_fixed_updated_date[0].updated_at.iso8601
     page.driver.post latest_hub_brands_path, {
       request_id: request_id,
       parameters: {
         limit: 40,
-        last_timestamp: 20.minutes.ago.iso8601,
-        last_id:  brands_with_recent_updated_date.third.id
+        last_timestamp: fixed_date,
+        last_id:  brands_with_fixed_updated_date.third.id
       }
     }
   end
 
   def then_i_should_not_get_the_brand_if_its_over_the_limit
     request_id_should_be_identical
-    expect(subject['brands'].count).to be(2)
-    expect(subject['brands'][0]['id']).to be(brands_with_recent_updated_date[3].id)
-    expect(subject['brands'][1]['id']).to be(brands_with_recent_updated_date[4].id)
+    brands_with_date_count = brands_with_recent_updated_date.count + brands_with_fixed_updated_date.count
+    brands_with_date_count -= 3 # to match the last_id sent
+    expect(subject['brands'].count).to be(brands_with_date_count)
+    expect(subject['brands'][0]['id']).to be(brands_with_fixed_updated_date[3].id)
+  end
+
+  def when_i_request_a_list_of_brands_which_is_not_returning_anything
+    create_brands
+    page.driver.post latest_hub_brands_path, {
+      request_id: request_id,
+      parameters: {
+        limit: 40,
+        last_timestamp: 2.days.since.iso8601,
+      }
+    }
+  end
+
+  def then_i_should_have_valid_data_returned
+    request_id_should_be_identical
+    expect(subject['brands'].count).to be(0)
+    expect(subject['parameters']['last_id'].to_i).to be(0)
+  end
+
+  def when_i_update_an_existing_brand_and_request_it
+    new_vendor
+    page.driver.post latest_hub_brands_path, {
+      request_id: request_id,
+      parameters: {
+        limit: 40,
+        last_timestamp: Time.now,
+      }
+    }
+  end
+
+  def then_i_should_have_the_updated_brand_only
+    request_id_should_be_identical
+    expect(subject['brands'].count).to be(1)
+    expect(subject['parameters']['last_id'].to_i).to be(new_vendor.id.to_i)
   end
 
   def request_id_should_be_identical
@@ -130,31 +196,37 @@ feature 'Listing Brands for the hub' do
     expect(subject['brands'][0]).to match(a_hash_including('id', 'name'))
   end
 
-  def when_i_request_a_list_of_brands_which_is_not_returning_anything
-    create_brands
-    page.driver.post latest_hub_brands_path, {
-      request_id: request_id,
-      parameters: {
-        limit: 40,
-        last_timestamp: 20.minutes.ago.iso8601,
-        last_id: create_brands.last.id + 1
-      }
-    }
+  def timestamp_returned_should_be_empty
+    expect(subject['parameters']['last_timestamp'].to_i).to be(0)
   end
 
-  def then_i_should_have_valid_data_returned
-    request_id_should_be_identical
-    expect(subject['brands'].count).to be(0)
-    expect(subject['parameters']['last_id'].to_i).to be(create_brands.last.id + 1)
+  def timestamp_should_be_recent
+    expect((Time.parse(subject['parameters']['last_timestamp']) - Time.now).abs < 10).to be(true)
   end
+
+  def last_id_should_be(last_id)
+    expect(subject['parameters']['last_id'].to_s).to eq(last_id.to_s)
+  end
+
+  def no_objects_should_be_returned
+    expect(subject['brands'].count).to be(0)
+  end
+
+  let(:new_vendor) { create(:vendor) }
 
   let(:create_brands) do
-    create_list(:vendor, 10, :without_updated_date)
+    brands_without_updated_date
     create_list(:vendor, 5, :with_old_updated_date)
     brands_with_recent_updated_date
+    brands_with_fixed_updated_date
   end
 
-  let(:brands_with_recent_updated_date) { create_list(:vendor, 5, :with_recent_updated_date) }
+  let(:brands_with_recent_updated_date) do
+    create_list(:vendor, 1, :with_recent_updated_date)
+  end
+
+  let(:brands_without_updated_date) { create_list(:vendor, 10, :without_updated_date) }
+  let(:brands_with_fixed_updated_date) { create_list(:vendor, 5, :with_fixed_updated_date) }
 
   let(:request_id) { Faker::Lorem.characters(15) }
 end
