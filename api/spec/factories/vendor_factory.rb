3b0e12d6690c7171574e92d4d1f56c454777c546
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-19 09:25:48 +0000

    Adding a new trait to create vendors with identical updated at date

--- api/spec/factories/vendor_factory.rb
+++ api/spec/factories/vendor_factory.rb
@@ -18,5 +18,10 @@ FactoryGirl.define do
     trait :with_recent_updated_date do
       updated_at 5.minutes.ago
     end
+
+    trait :with_fixed_updated_date do
+      # does not matter which date it is as long as it's the same
+      updated_at Time.parse('2016-02-18T17:26:26')
+    end
   end
 end
