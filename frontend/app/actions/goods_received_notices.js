b39563f994ab99588938c5dea22452decaa35eb5
Luke <lukemorton.dev@gmail.com>
2016-02-19 15:13:02 +0000

    Revert "Showing only 2 weeks before and 2 weeks after"
    This reverts commit 45762e39db073c3ddaf69a2eb4033494c0d8402e.

--- frontend/app/actions/goods_received_notices.js
+++ frontend/app/actions/goods_received_notices.js
@@ -26,7 +26,7 @@ export function loadGoodsReceivedNotice(id) {
 
 export function loadGoodsReceivedNotices(middleWeekStart) {
   const startDate = moment(middleWeekStart, 'DD/MM/YYYY').subtract(2, 'weeks').format('YYYY-MM-DD');
-  const endDate = moment(middleWeekStart, 'DD/MM/YYYY').add({ weeks: 2 }).format('YYYY-MM-DD');
+  const endDate = moment(middleWeekStart, 'DD/MM/YYYY').add({ weeks: 2, days: 5 }).format('YYYY-MM-DD');
 
   return dispatch => {
     const queryString = Qs.stringify({ start_date: startDate, end_date: endDate });
