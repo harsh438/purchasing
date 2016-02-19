786268dd22e4647e13ca7292a7ab8d5dd0d41b0b
Luke <lukemorton.dev@gmail.com>
2016-02-19 13:59:05 +0000

    Strip barcode of whitespace

--- api/app/models/barcode.rb
+++ api/app/models/barcode.rb
@@ -1,4 +1,12 @@
 class Barcode < ActiveRecord::Base
   belongs_to :sku
   validates :barcode, presence: true
+
+  before_validation :strip_whitespace
+
+  private
+
+  def strip_whitespace
+    self.barcode = barcode.strip if barcode.present?
+  end
 end
