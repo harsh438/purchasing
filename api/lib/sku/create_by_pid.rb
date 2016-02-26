466aee62ea57f8aa07a9b32cd30bf16d5b2df155
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-26 16:01:30 +0000

    Starting the logic of creating new skus

--- api/lib/sku/create_by_pid.rb
+++ api/lib/sku/create_by_pid.rb
@@ -1,5 +1,26 @@
 class Sku::CreateByPid
-  def create(pid)
-    product = Product.find(pid.to_i)
+  def create(options = {})
+    options.slice!(:product_id, :element_id)
+    product = Product.find(options[:product_id].to_i)
+    element = Element.find(options[:element_id].to_i)
+    last_sku = product.skus.order(:created_at).last
+    new_sku = copy_sku(last_sku)
+    new_sku[:sku] = "#{product.id}-#{element.name}"
+    Sku.create(new_sku)
+  end
+
+  private
+
+  def copy_sku(sku)
+    sku.as_json.symbolize_keys.except(
+      :id,
+      :sku,
+      :created_at,
+      :updated_at,
+      :barcode,
+      :size,
+      :element_id,
+      :manufacturer_size
+    )
   end
 end
