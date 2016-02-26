6a80d010331ca6ae1f285e367e73aa09d22735a6
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-26 08:45:48 +0000

    Adding pid field to the form

--- frontend/app/views/skus/new.jsx
+++ frontend/app/views/skus/new.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SkusNew extends React.Component {
+  componentWillMount() {
+    this.state = {};
+  }
+
   render() {
-    return (<div className="skus_new container-fluid application-root-container">
+    return (
+    <div className="skus_new container container-fluid application-root-container">
+      <h1>Add New SKU</h1>
+      <div className="panel panel-default">
+        <div className="panel-body">
+          {this.renderForm()}
+        </div>
+      </div>
     </div>);
+  }
+
+  renderForm() {
+    return (
+      <form className="form">
+        <label htmlFor="product_id">PID</label>
+        <input className="form-control"
+               id="product_id"
+               name="invoicer_name"
+               placeholder="PID associated with the SKU"
+               value={this.state.product_id} />
+      </form>
+    );
   }
 }
 
