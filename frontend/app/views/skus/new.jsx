ebfc8f62e30441f7c095abf1b453b341024dd360
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-26 11:01:22 +0000

    Mapping the new sku interface to the create by pid call

--- frontend/app/views/skus/new.jsx
+++ frontend/app/views/skus/new.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import NotificationSystem from 'react-notification-system';
 import { connect } from 'react-redux';
+import { assign } from 'lodash';
+import { createSkuByPid } from '../../actions/skus';
+import { processNotifications } from '../../utilities/notification';
 
 class SkusNew extends React.Component {
   componentWillMount() {
-    this.state = {};
+    this.state = { sku: {} };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ submitting: false });
+    processNotifications.call(this, nextProps);
   }
 
   render() {
@@ -20,20 +29,47 @@ class SkusNew extends React.Component {
 
   renderForm() {
     return (
-      <form className="form">
-        <label htmlFor="product_id">PID</label>
-        <input className="form-control"
-               id="product_id"
-               name="invoicer_name"
-               placeholder="PID associated with the SKU"
-               value={this.state.product_id} />
-      </form>
+      <div>
+        <NotificationSystem ref="notificationSystem" />
+        <form className="form"
+              onChange={this.handleFormChange.bind(this)}
+              onSubmit={this.handleFormSubmit.bind(this)}>
+          <label htmlFor="product_id">PID</label>
+          <input type="number"
+                 className="form-control"
+                 id="productId"
+                 name="productId"
+                 placeholder="PID associated with the SKU"
+                 value={this.state.sku.productId} />
+          <br />
+          {this.renderSubmitButton()}
+        </form>
+      </div>
     );
+  }
+
+  renderSubmitButton() {
+    if (this.state.submitting) {
+      return (<input type="submit" className="btn btn-success" value="Creating New SKU..." disabled />);
+    } else {
+      return (<input type="submit" className="btn btn-success" value="Create New SKU" />);
+    }
+  }
+
+  handleFormChange ({ target }) {
+    const sku = assign({}, this.state.sku, { [target.name]: target.value });
+    this.setState({ sku });
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    this.setState({ submitting: true });
+    this.props.dispatch(createSkuByPid(this.state.sku));
   }
 }
 
-function applyState({ suppliers }) {
-  return suppliers;
+function applyState({ skus, notification }) {
+  return assign({}, skus, notification);;
 }
 
 export default connect(applyState)(SkusNew);
