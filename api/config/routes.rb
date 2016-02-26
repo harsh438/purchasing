29c4594eb91f2290006758cab2f77905bc254697
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-26 08:45:04 +0000

    Adding new create by pid route

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :skus, only: [:index, :create, :show, :update] do
       collection do
         get :supplier_summary
+        post :create_by_pid
       end
     end
 
