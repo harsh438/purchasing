4b2444b512e6cf26834c5bf49d8c9f8b266eef2d
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-29 12:08:05 +0000

    Refactoring

--- api/lib/sku/create_by_pid.rb
+++ api/lib/sku/create_by_pid.rb
@@ -4,15 +4,18 @@ class Sku::CreateByPid
     product = Product.find(options[:product_id].to_i)
     element = Element.find(options[:element_id].to_i)
     last_sku = product.skus.order(:created_at).last
-    sku_attrs = copy_sku_attributes(last_sku, product, element)
-    sku = Sku::Generator.new.generate_new_sku(sku_attrs)
+    sku = Sku::Generator.new.generate_new_sku(copy_sku_attributes(last_sku, product, element))
+    save_sku(sku, product, last_sku)
+  end
+
+  private
+  def save_sku(sku, product, last_sku)
     sku.product_id = product.id
     sku.language_product_id = last_sku.language_product_id
     sku.save!
     sku
   end
 
-  private
   def copy_sku_attributes(sku, product, element)
     sku_attrs = sku.as_json.symbolize_keys.except(:barcode, :manufacturer_size)
     sku_attrs[:internal_sku] = "#{product.id}-#{element.name}"
