293f84c4e58814fa8cd21319fa3156902d606666
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-03-01 12:12:22 +0000

    Updating the frontend to add the update barcode form

--- frontend/app/views/skus/_barcode_table.jsx
+++ frontend/app/views/skus/_barcode_table.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { map } from 'lodash';
 
 export default class SkusBarcodeTable extends React.Component {
+  componentWillMount() {
+    this.state = { editing: false };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ editing: false });
+  }
+
   render() {
     return (
       <div className="panel panel-default">
@@ -26,8 +34,48 @@ export default class SkusBarcodeTable extends React.Component {
   renderRow(barcode) {
     return (
       <tr>
-        <td>{barcode.barcode}</td>
+        <td>
+          {this.renderBarcode(barcode)}
+        </td>
       </tr>
     );
+  }
+
+  renderBarcode(barcode) {
+    if (this.state.editing) {
+      return this.renderWithEdit(barcode);
+    } else {
+      return this.renderWithoutEdit(barcode);
+    }
+  }
+
+  renderWithoutEdit(barcode) {
+    return (
+      <div>
+        <div className="col-md-10">{barcode.barcode}</div>
+        <div className="col-md-2">
+          <button onClick={this.handleEditButton.bind(this)} className="btn btn-success pull-right">
+            <i className="glyphicon glyphicon-edit"></i>&nbsp;Edit
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  renderWithEdit(barcode) {
+    return (
+      <form className="form">
+        <div className="form-group col-md-10">
+          <input className="form-control" type="text" value={barcode.barcode} />
+        </div>
+        <div className="form-group col-md-2">
+          <input type="submit" value="Save" className="btn btn-success" />
+        </div>
+      </form>
+    );
+  }
+
+  handleEditButton() {
+    this.setState({ editing: true });
   }
 }
