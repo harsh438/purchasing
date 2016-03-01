b705443483b84f2c505b79a1dda05b4095d27c31
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-03-01 15:21:29 +0000

    Returning the list of barcodes instead of the single barcode changed

--- api/lib/barcode/updater.rb
+++ api/lib/barcode/updater.rb
@@ -1,12 +1,19 @@
 class Barcode::Updater
   def self.update(params)
-    barcode = Barcode.find_by(barcode: params[:old_barcode])
-    barcode.barcode = params[:new_barcode]
+    self.check_barcode_uniqueness(params[:barcode])
+    barcode = Barcode.find(params[:id])
+    barcode.barcode = params[:barcode]
     ActiveRecord::Base.transaction do
       barcode.save!
       self.update_barcode_references(barcode)
     end
-    barcode
+    barcode.sku.barcodes
+  end
+
+  def self.check_barcode_uniqueness(barcode)
+    if Barcode.find_by(barcode: barcode).present?
+      raise Exceptions::BarcodeUpdateError, "Barcode #{barcode} is not unique"
+    end
   end
 
   private
