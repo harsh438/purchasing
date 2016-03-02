7e55476e603ebdb05e88fe97995358db40af693d
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-03-02 11:37:18 +0000

    Checking if the barcode is editable

--- api/lib/barcode/updater.rb
+++ api/lib/barcode/updater.rb
@@ -2,12 +2,20 @@ class Barcode::Updater
   def self.update(params)
     self.check_barcode_uniqueness(params[:barcode])
     barcode = Barcode.find(params[:id])
+    self.check_editable(barcode)
     barcode.barcode = params[:barcode]
     ActiveRecord::Base.transaction do
       barcode.save!
       self.update_barcode_references(barcode)
     end
     barcode.sku.barcodes
+  end
+
+  private
+  def self.check_editable(barcode)
+    unless barcode.sku.sized?
+      raise Exceptions::BarcodeUpdateError, "Unsized skus are not editable."
+    end
   end
 
   def self.check_barcode_uniqueness(barcode)
@@ -17,7 +25,6 @@ class Barcode::Updater
     end
   end
 
-  private
   def self.update_barcode_references(barcode)
     PurchaseOrderLineItem.where(sku: barcode.sku).update_all(orderTool_Barcode: barcode.barcode)
   end
