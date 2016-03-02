2ea09e3e706840f2addf3e97757c24a1866d6615
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-03-02 15:31:11 +0000

    Adding the test for empty manufacturer size

--- api/spec/features/skus/size_update_spec.rb
+++ api/spec/features/skus/size_update_spec.rb
@@ -6,6 +6,11 @@ feature 'Editing manufacturer size' do
     then_associated_option_should_be_updated
   end
 
+  scenario 'Manufacturer size cannot be empty' do
+    when_i_update_manufacturer_size_to_empty_string
+    then_update_should_be_refused
+  end
+
   def when_i_update_manufacturer_size
     page.driver.post sku_path(sku), {
       _method: 'PATCH',
@@ -21,6 +26,22 @@ feature 'Editing manufacturer size' do
     expect(sku.option.size).to eq(new_manufacturer_size)
   end
 
+  def when_i_update_manufacturer_size_to_empty_string
+    page.driver.post sku_path(sku), {
+      _method: 'PATCH',
+      sku: {
+        id: sku.id,
+        manufacturer_size: empty_manufacturer_size
+      }
+    }
+  end
+
+  def then_update_should_be_refused
+    expect(page.driver.status_code).to eq(422)
+    expect(subject['message']).to eq('Manufacturer size cannot be empty')
+  end
+
   let(:new_manufacturer_size) { Faker::Lorem.characters(15) }
+  let(:empty_manufacturer_size) { "    " }
   let(:sku) { create(:sku) }
 end
