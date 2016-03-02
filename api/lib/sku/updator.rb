caf4881e22891444756374ad8f8bff763c0b73d7
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-03-02 15:30:44 +0000

    Now checking that the manufacturer size is not empty

--- api/lib/sku/updator.rb
+++ api/lib/sku/updator.rb
@@ -1,6 +1,7 @@
 class Sku::Updator
   def self.update(sku, params)
     ActiveRecord::Base.transaction do
+      self.check_empty_size(params)
       sku.update!(params)
       Sku::Exporter.new.export(sku)
       self.update_option(sku, params) if params.include?(:manufacturer_size)
@@ -10,5 +11,11 @@ class Sku::Updator
   private
   def self.update_option(sku, params)
     sku.option.update!(size: params[:manufacturer_size])
+  end
+
+  def self.check_empty_size(params)
+    if params.include?(:manufacturer_size) and params[:manufacturer_size].strip.empty?
+      raise Exceptions::SkuUpdateError, "Manufacturer size cannot be empty"
+    end
   end
 end
