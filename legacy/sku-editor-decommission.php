13cad5f4f77a2b388288cb65a98d4d7bb6671bd8
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-03-03 17:12:15 +0000

    Adding a decommission page for the sku editor

---
+++ legacy/sku-editor-decommission.php
@@ -0,0 +1,22 @@
+<?php
+
+# SKU EDITOR tool is has now been decommissioned
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tool decommissioned</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<body style="background-color:rgb(238, 238, 243)">
+  <center style="margin-top:100px">
+    <h1>
+      The SKU Editor tool has now been decommissioned<br>
+      Please use the purchasing app to edit/create SKUs instead.
+    </h1>
+  </center>
+</body>
+</html>
+EOT;
+exit;
+
