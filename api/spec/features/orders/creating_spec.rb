ea56291e8afef83aa90d08b2b5a1cf6b54e59361
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-03-08 12:07:40 +0000

    Removing old test wich was not using seasons

--- api/spec/features/orders/creating_spec.rb
+++ api/spec/features/orders/creating_spec.rb
@@ -1,11 +1,6 @@
 feature 'Creating order' do
   subject { JSON.parse(page.body) }
 
-  scenario 'Re-ordering stock' do
-    when_i_want_to_reorder_certain_products
-    then_i_should_be_able_to_create_a_new_order
-  end
-
   scenario 'Re-ordering stock with season' do
     when_i_want_to_reorder_certain_products_with_a_season
     then_i_should_be_able_to_create_a_new_order_with_a_season
@@ -13,12 +8,6 @@ feature 'Creating order' do
 
   def when_i_want_to_reorder_certain_products
     page.driver.post orders_path(order: { name: 'Nice order' })
-  end
-
-  def then_i_should_be_able_to_create_a_new_order
-    expect(subject).to include('id' => a_kind_of(Integer))
-    expect(subject).to include('name' => 'Nice order')
-    expect(subject).to include('status' => 'new')
   end
 
   def when_i_want_to_reorder_certain_products_with_a_season
