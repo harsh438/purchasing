018e9f233bab44b7761cd40627cd35eb396ec0bc
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-03-11 12:34:08 +0000

    Unsized skus barcodes are now editable

--- api/lib/barcode/updater.rb
+++ api/lib/barcode/updater.rb
@@ -2,7 +2,6 @@ class Barcode::Updater
   def self.update(params)
     self.check_barcode_uniqueness(params[:barcode])
     barcode = Barcode.find(params[:id])
-    self.check_editable(barcode)
     barcode.barcode = params[:barcode]
     ActiveRecord::Base.transaction do
       barcode.save!
@@ -12,12 +11,6 @@ class Barcode::Updater
   end
 
   private
-  def self.check_editable(barcode)
-    unless barcode.sku.sized?
-      raise Exceptions::BarcodeUpdateError, 'Unsized skus are not editable.'
-    end
-  end
-
   def self.check_barcode_uniqueness(barcode)
     barcode = Barcode.find_by(barcode: barcode)
     if barcode.present?
