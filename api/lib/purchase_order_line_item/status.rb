ec0dac087aedd3908e8008c01a9834a51120a368
Luke <lukemorton.dev@gmail.com>
2016-03-11 12:45:56 +0000

    Whitespace

--- api/lib/purchase_order_line_item/status.rb
+++ api/lib/purchase_order_line_item/status.rb
@@ -1,31 +1,31 @@
 class PurchaseOrderLineItem::Status
   def self.sym_from_int(i)
     case i
-      when -1
-        :cancelled
-      when 2
-        :new_po
-      when 3
-        :receiving
-      when 4
-        :balance
-      when 5
-        :delivered
-      else
-        :error
+    when -1
+      :cancelled
+    when 2
+      :new_po
+    when 3
+      :receiving
+    when 4
+      :balance
+    when 5
+      :delivered
+    else
+      :error
     end
   end
 
   def self.ints_from_filter_sym(s)
     case s
-      when :cancelled
-        [-1]
-      when :balance
-        [2, 4]
-      when :received
-        [-1, 3, 4, 5]
-      else
-        [-1, 2, 3, 4, 5]
+    when :cancelled
+      [-1]
+    when :balance
+      [2, 4]
+    when :received
+      [-1, 3, 4, 5]
+    else
+      [-1, 2, 3, 4, 5]
     end
   end
 
