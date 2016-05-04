fba55f872b0eec044fac5aa5b5f0f17f06830012
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-05-04 18:04:11 +0100

    Allow manufaturer size if the update is not significant. Solving https://app.getsentry.com/surfdome/purchasing/issues/115588532/

--- api/lib/sku/updator.rb
+++ api/lib/sku/updator.rb
@@ -1,7 +1,7 @@
 class Sku::Updator
   def self.update(sku, params)
     ActiveRecord::Base.transaction do
-      self.check_empty_size(params)
+      self.check_empty_size(params, sku)
       sku.update!(params)
       Sku::Exporter.new.export(sku)
       self.update_option(sku, params) if params.include?(:manufacturer_size)
@@ -16,8 +16,10 @@ class Sku::Updator
     end
   end
 
-  def self.check_empty_size(params)
-    if params.include?(:manufacturer_size) and params[:manufacturer_size].strip.empty?
+  def self.check_empty_size(params, sku)
+    size = (params[:manufacturer_size] || '').strip
+    old_size = (sku.manufacturer_size || '').strip
+    if params.include?(:manufacturer_size) and size.empty? and old_size.present?
       raise Exceptions::SkuUpdateError, 'Manufacturer size cannot be empty'
     end
   end
--- api/spec/features/skus/size_update_spec.rb
+++ api/spec/features/skus/size_update_spec.rb
@@ -11,6 +11,11 @@ feature 'Editing manufacturer size' do
     then_update_should_be_refused
   end
 
+  scenario 'Manufacturer size cannot be empty except if it was empty before' do
+    when_i_update_manufacturer_size_to_empty_string_with_empty_before
+    then_associated_option_should_be_updated_to_nothing
+  end
+
   def when_i_update_manufacturer_size
     page.driver.post sku_path(sku), {
       _method: 'PATCH',
@@ -22,8 +27,8 @@ feature 'Editing manufacturer size' do
   end
 
   def then_associated_option_should_be_updated
-    expect(subject['manufacturer_size']).to eq(new_manufacturer_size)
-    expect(sku.option.size).to eq(new_manufacturer_size)
+    expect(subject['manufacturer_size']).to eq(new_manufacturer_size.strip)
+    expect(sku.option.size).to eq(new_manufacturer_size.strip)
   end
 
   def when_i_update_manufacturer_size_to_empty_string
@@ -41,7 +46,23 @@ feature 'Editing manufacturer size' do
     expect(subject['message']).to eq('Manufacturer size cannot be empty')
   end
 
+  def when_i_update_manufacturer_size_to_empty_string_with_empty_before
+    page.driver.post sku_path(sku_with_empty_size), {
+      _method: 'PATCH',
+      sku: {
+        id: sku_with_empty_size.id,
+        manufacturer_size: empty_manufacturer_size
+      }
+    }
+  end
+
+  def then_associated_option_should_be_updated_to_nothing
+    expect(subject['manufacturer_size']).to eq(empty_manufacturer_size)
+    expect(sku_with_empty_size.option.size).to eq(empty_manufacturer_size)
+  end
+
   let(:new_manufacturer_size) { Faker::Lorem.characters(15) }
   let(:empty_manufacturer_size) { '    ' }
   let(:sku) { create(:sku) }
+  let(:sku_with_empty_size) { create(:sku, manufacturer_size: nil) }
 end
