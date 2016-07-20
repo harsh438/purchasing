b8092dff9de91f2e2a0993abc7666d22e35e3fbc
Richard Foster <richard@madetech.co.uk>
2016-07-20 09:25:44 +0100

    Don't mark GRN Event as received if it already has been

--- api/spec/features/goods_received_notices/marking_received_spec.rb
+++ api/spec/features/goods_received_notices/marking_received_spec.rb
@@ -6,6 +6,11 @@ feature 'Marking Purchase Order as received', booking_db: true do
     then_grn_should_report_received_units
   end
 
+  scenario 'Marking PO as received when already received' do
+    when_a_received_purchase_order_is_received
+    then_grn_received_units_should_not_change
+  end
+
   def when_a_purchase_order_is_received
     page.driver.post goods_received_notice_path(grn), { _method: 'patch',
                                                         goods_received_notice: grn_attrs }
@@ -21,14 +26,23 @@ feature 'Marking Purchase Order as received', booking_db: true do
     expect(subject['cartons_received']).to eq(grn.goods_received_notice_events.first.cartons)
   end
 
+  def when_a_received_purchase_order_is_received
+    grn_event.received = true
+    page.driver.post goods_received_notice_path(grn), { _method: 'patch',
+                                                        goods_received_notice: grn_attrs }
+  end
+
+  def then_grn_received_units_should_not_change
+    expect(subject['units_received']).to eq(grn.goods_received_notice_events.first.units)
+    expect(subject['cartons_received']).to eq(grn.goods_received_notice_events.first.cartons)
+  end
+
   private
 
   let(:grn) { create(:goods_received_notice, :with_purchase_orders) }
-
   let(:grn_event) { grn.goods_received_notice_events.first }
 
   let(:grn_attrs) do
-    { goods_received_notice_events_attributes: [{ id: grn_event.id,
-                                                  received: true }] }
+    { goods_received_notice_events_attributes: [{ id: grn_event.id, received: true }] }
   end
 end
