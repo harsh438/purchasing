0263a9e4b0d20353e5d913499db8a1ad1ba01436
Edward Withers <edward.withers@surfdome.com>
2016-11-11 15:17:23 +0000

    Add OVER PO support for generating skus with missing PO season (#171)
    * Update Over PO controller to support generating new skus for missing seasons
    
    - add in season to skunotfound exception
    - handle errors as needed
    - generates sku with same sku, barcode, and uses PO season
    
    * tailor fix
    
    * remove sloppiness
    
    * remove sloppiness again
    
    * code climate clean up
    
    * extract common expectations, remove redundant code
    
    * wrap over_delivery#generate in transaction to avoid creating objects before some failure

--- api/app/controllers/over_purchase_orders_controller.rb
+++ api/app/controllers/over_purchase_orders_controller.rb
@@ -2,7 +2,7 @@ class OverPurchaseOrdersController < ApplicationController
   rescue_from ActionController::ParameterMissing, ActiveRecord::RecordInvalid,
     with: :handle_error
   rescue_from Order::SkuNotFound,
-    with: :handle_sku_not_found
+    with: :sku_not_found
 
   def create
     over_delivery = OverDelivery::Generator.new(over_delivery_attrs).generate
@@ -12,7 +12,7 @@ class OverPurchaseOrdersController < ApplicationController
   private
 
   def over_delivery_attrs
-    %i(sku quantity user_id grn po_numbers).each_with_object({}) do |attribute, hash|
+    @attrs ||= %i(sku quantity user_id grn po_numbers).each_with_object({}) do |attribute, hash|
       hash[attribute] = params.require(:over_po).require(attribute)
     end
   end
@@ -21,7 +21,32 @@ class OverPurchaseOrdersController < ApplicationController
     render json: { summary: e.message }, status: 500
   end
 
-  def handle_sku_not_found
-    render json: { summary: "Internal SKU does not exist for any season: #{over_delivery_attrs[:sku]}" }
+  def sku_not_found(e)
+    sku, season = e.sku, e.season
+    begin
+      old_sku = Sku.find_by!(sku: sku)
+      generate_sku_and_reprocess(old_sku, season)
+    rescue ActiveRecord::RecordNotFound => e
+      handle_error(e)
+    end
+  end
+
+  def generate_sku_and_reprocess(old_sku, season)
+    begin
+      barcode = Barcode.find_by!(sku_id: old_sku.id).barcode
+      attrs = new_sku_attrs(old_sku, barcode, season)
+      Sku::Generator.new.generate(attrs)
+      create
+    rescue ActiveRecord::RecordNotFound => e
+      handle_error(e)
+    end
+  end
+
+  def new_sku_attrs(sku, barcode, season)
+    attrs = sku.attributes
+    attrs[:season] = season
+    attrs[:barcode] = barcode
+    attrs[:internal_sku] = attrs['sku']
+    attrs
   end
 end
--- api/spec/controllers/over_purchase_orders_controller_spec.rb
+++ api/spec/controllers/over_purchase_orders_controller_spec.rb
@@ -4,13 +4,9 @@ RSpec.describe OverPurchaseOrdersController do
   include JSONFixture
 
   let(:over_po) { fixture('spec/fixtures/files/sample_over_po.json') }
-  let(:internal_sku_error_message) do
-    'Internal SKU does not exist for any season: ' \
-    "#{over_po.obj['over_po']['sku']}"
-  end
 
   describe 'create' do
-    context 'with valid sku' do
+    context 'with correct sku' do
       let!(:purchase_order) do
         create(:purchase_order_line_item,
                :with_summary,
@@ -28,27 +24,39 @@ RSpec.describe OverPurchaseOrdersController do
       end
       let(:last_po) { PurchaseOrderLineItem.last }
 
-      it 'with attributes' do
-        post :create, over_po.obj.merge(format: :json)
-        data = JSON.parse(response.body).first
-        exports = data['exports']
-        expect(data['status']).to eq 'exported'
-        expect(data['order_type']).to eq 'over_po'
-        expect(exports.first['purchase_order_id']).to eq last_po.id
-        expect(data['line_items'].count).to eq exports.count
-        expect(last_po.attributes.fetch('status')).to eq 2
-        expect(last_po.quantity_done).to eq 0
-        expect(last_po.operator).to eq 'O_U_TOOL'
-        expect(last_po.id).to eq exports.first['purchase_order_id']
-        expect(data['name']).to eq "OVER_#{OverDelivery.last.id}"
+      context 'correct season' do
+        it 'generates over po' do
+          post :create, over_po.obj.merge(format: :json)
+          data = JSON.parse(response.body).first
+          expect_over_po_data_to_have_correct_info(data)
+        end
+      end
+
+      context 'wrong season' do
+        before { sku.update(season: Season.all[4]) }
+
+        context 'correct barcode' do
+          it 'generates sku for correct season, and generates over po' do
+            expect { post :create, over_po.obj.merge(format: :json) }.to change(Sku, :count).by(1)
+            data = JSON.parse(response.body).first
+            expect_over_po_data_to_have_correct_info(data)
+          end
+        end
+        context 'no barcode' do
+          let(:sku) { create(:base_sku, :with_product, sku: over_po.obj['over_po']['sku']) }
+
+          it 'raises  error' do
+            message = 'Couldn\'t find Barcode'
+            expect_create_to_handle_error_with(over_po.obj, message)
+          end
+        end
       end
     end
 
     context 'with invalid sku' do
       it 'raises caught internal sku error' do
-        post :create, over_po.obj.merge(format: :json)
-        data = JSON.parse(response.body)
-        expect(data['summary']).to eq internal_sku_error_message
+        message = 'Couldn\'t find Sku'
+        expect_create_to_handle_error_with(over_po.obj, message)
       end
     end
 
@@ -58,9 +66,8 @@ RSpec.describe OverPurchaseOrdersController do
 
       it 'error is handled' do
         allow(OverDelivery).to receive(:create!).and_raise(ActiveRecord::RecordInvalid, over_delivery)
-        post :create, over_po.obj.merge(format: :json)
-        data = JSON.parse(response.body)
-        expect(data['summary']).to eq 'Validation failed: Sku some random error here'
+        message = 'Validation failed: Sku some random error here'
+        expect_create_to_handle_error_with(over_po.obj, message)
       end
     end
 
@@ -73,12 +80,30 @@ RSpec.describe OverPurchaseOrdersController do
             end.obj[0]
           end
           it "raises error message" do
-            post :create, over_po.merge(format: :json)
-            data = JSON.parse(response.body)
-            expect(data['summary']).to eq "param is missing or the value is empty: #{attribute}"
+            message = "param is missing or the value is empty: #{attribute}"
+            expect_create_to_handle_error_with(over_po, message)
           end
         end
       end
     end
   end
+end
+
+def expect_over_po_data_to_have_correct_info(data)
+  exports = data['exports']
+  expect(data['status']).to eq 'exported'
+  expect(data['order_type']).to eq 'over_po'
+  expect(exports.first['purchase_order_id']).to eq last_po.id
+  expect(data['line_items'].count).to eq exports.count
+  expect(last_po.attributes.fetch('status')).to eq 2
+  expect(last_po.quantity_done).to eq 0
+  expect(last_po.operator).to eq 'O_U_TOOL'
+  expect(last_po.id).to eq exports.first['purchase_order_id']
+  expect(data['name']).to eq "OVER_#{OverDelivery.last.id}"
+end
+
+def expect_create_to_handle_error_with(over_po, message)
+  post :create, over_po.merge(format: :json)
+  data = JSON.parse(response.body)
+  expect(data['summary']).to eq message
 end
